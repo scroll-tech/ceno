ae39cbde9337dd166821e80bdeaf185feb21d7d2
zhenfei <zhenfei.zhang@hotmail.com>
2023-11-09 15:45:50 -0500

    init and implement goldilocks

---
+++ goldilocks/src/lib.rs
@@ -0,0 +1,8 @@
+//! This crate implements Goldilocks field with modulus 2^64 - 2^32 + 1
+//! Credit: the majority of the code is borrowed or inspired from Plonky2 with modifications.
+
+mod fp;
+mod util;
+
+#[cfg(test)]
+mod tests;
