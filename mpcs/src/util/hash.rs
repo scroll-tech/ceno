edb4fc9b483fd987e7b579a6cbfc061206766d2f
Yuncong Zhang <yczhangsjtu@gmail.com>
2024-01-17 13:47:07 +0800

    Temporarily switch to plonky2 for Poseidon.

--- mpcs/src/util/hash.rs
+++ mpcs/src/util/hash.rs
@@ -1,5 +1,13 @@
 use goldilocks::SmallField;
 
+use plonky2::{
+    field::{
+        goldilocks_field::GoldilocksField,
+        types::{Field, PrimeField64},
+    },
+    hash::{hash_types::HashOut, poseidon::PoseidonHash},
+    plonk::config::Hasher as _,
+};
 use poseidon::Poseidon;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
@@ -23,20 +31,61 @@ pub fn hash_two_leaves<F: SmallField>(a: &F, b: &F) -> Digest<F>
 where
     F::BaseField: Serialize + DeserializeOwned,
 {
-    let mut hasher = new_hasher::<F>();
-    hasher.update(a.to_limbs().as_slice());
-    hasher.update(b.to_limbs().as_slice());
-    let result = hasher.squeeze_vec()[0..DIGEST_WIDTH].try_into().unwrap();
-    Digest(result)
+    let repr = a
+        .to_canonical_u64_vec()
+        .iter()
+        .chain(b.to_canonical_u64_vec().iter())
+        .map(|x| GoldilocksField::from_canonical_u64(*x))
+        .collect::<Vec<GoldilocksField>>();
+
+    let result = PoseidonHash::hash_no_pad(repr.as_slice());
+    Digest(
+        result
+            .elements
+            .iter()
+            .map(|x| F::BaseField::from(x.to_canonical_u64()))
+            .collect::<Vec<_>>()
+            .as_slice()
+            .try_into()
+            .unwrap(),
+    )
+
+    // let mut hasher = new_hasher::<F>();
+    // hasher.update(a.to_limbs().as_slice());
+    // hasher.update(b.to_limbs().as_slice());
+    // let result = hasher.squeeze_vec()[0..DIGEST_WIDTH].try_into().unwrap();
+    // Digest(result)
 }
 
 pub fn hash_two_digests<F: SmallField>(a: &Digest<F>, b: &Digest<F>) -> Digest<F>
 where
     F::BaseField: Serialize + DeserializeOwned,
 {
-    let mut hasher = new_hasher::<F>();
-    hasher.update(a.0.as_slice());
-    hasher.update(b.0.as_slice());
-    let result = hasher.squeeze_vec()[0..DIGEST_WIDTH].try_into().unwrap();
-    Digest(result)
+    let a = HashOut::<GoldilocksField>::from_vec(
+        a.0.iter()
+            .map(|x| GoldilocksField::from_canonical_u64(x.to_canonical_u64_vec()[0]))
+            .collect::<Vec<_>>(),
+    );
+    let b = HashOut::<GoldilocksField>::from_vec(
+        b.0.iter()
+            .map(|x| GoldilocksField::from_canonical_u64(x.to_canonical_u64_vec()[0]))
+            .collect::<Vec<_>>(),
+    );
+
+    let result = PoseidonHash::two_to_one(a, b);
+    Digest(
+        result
+            .elements
+            .iter()
+            .map(|x| F::BaseField::from(x.to_canonical_u64()))
+            .collect::<Vec<_>>()
+            .as_slice()
+            .try_into()
+            .unwrap(),
+    )
+    // let mut hasher = new_hasher::<F>();
+    // hasher.update(a.0.as_slice());
+    // hasher.update(b.0.as_slice());
+    // let result = hasher.squeeze_vec()[0..DIGEST_WIDTH].try_into().unwrap();
+    // Digest(result)
 }
