639ee93f44d723c31a2c3edcb3782b6b2a0210d6
huwenqing0606 <huwenqing.pku@gmail.com>
2024-01-28 22:15:52 -0600

    scheme of tests of proof verification for opcodes, start from push

--- singer/src/instructions/push.rs
+++ singer/src/instructions/push.rs
@@ -218,3 +218,126 @@ impl<const N: usize> Instruction for PushInstruction<N> {
         })
     }
 }
+
+
+#[cfg(test)]
+mod test{
+    use core::ops::Range;
+    use std::collections::BTreeMap;
+
+    use goldilocks::Goldilocks;
+    use crate::instructions::{ChipChallenges, Instruction, PushInstruction};
+    use gkr::structs::CircuitWitness;
+
+    impl<const N: usize> PushInstruction<N> {
+        #[inline]
+        fn phase0_sizes() -> BTreeMap<String, Range<usize>> {
+            let mut map = BTreeMap::new();
+            map.insert("phase0_pc".to_string(), Self::phase0_pc());
+            map.insert("phase0_stack_ts".to_string(), Self::phase0_stack_ts());
+            map.insert("phase0_stack_top".to_string(), Self::phase0_stack_top());
+            map.insert("phase0_clk".to_string(), Self::phase0_clk());
+            map.insert("phase0_pc_add_i_plus_1".to_string(), Self::phase0_pc_add_i_plus_1());
+            map.insert("phase0_stack_ts_add".to_string(), Self::phase0_stack_ts_add());
+            map.insert("phase0_stack_bytes".to_string(), Self::phase0_stack_bytes());
+            
+            map
+        }
+
+        #[inline]
+        fn phase1_sizes() -> BTreeMap<String, Range<usize>> {
+            let mut map = BTreeMap::new();
+            map.insert("phase1_memory_ts_rlc".to_string(), Self::phase1_memory_ts_rlc());
+            
+            map
+        }
+    }
+
+
+    #[test]
+    fn test_push1_construct_circuit() {
+        let chip_challenges = ChipChallenges::default();
+        
+        // configure a push1 circuit
+        let push1_InstCircuit 
+            = PushInstruction::<1>::construct_circuit::<Goldilocks>(chip_challenges).unwrap();
+        let circuit = push1_InstCircuit.circuit.as_ref();
+        
+        // get indexes for circuit inputs and wire_in
+        // they are divided into phase0 and phase1
+        let inputs_idxes = &push1_InstCircuit.layout.phases_wire_id;
+        let phase0_input_idx = inputs_idxes[0].unwrap();
+        let phase1_input_idx = inputs_idxes[1].unwrap();
+
+        let phase0_idx_map = PushInstruction::<1>::phase0_sizes();
+        let phase1_idx_map = PushInstruction::<1>::phase1_sizes();
+        let phase0_pc = phase0_idx_map.get("phase0_pc").unwrap();
+        let phase0_stack_ts = phase0_idx_map.get("phase0_stack_ts").unwrap();
+        let phase0_stack_top = phase0_idx_map.get("phase0_stack_top").unwrap();
+        let phase0_clk = phase0_idx_map.get("phase0_clk").unwrap();
+        let phase0_pc_add_i_plus_1 = phase0_idx_map.get("phase0_pc_add_i_plus_1").unwrap();
+        let phase0_stack_ts_add = phase0_idx_map.get("phase0_stack_ts_add").unwrap();
+        let phase0_stack_bytes = phase0_idx_map.get("phase0_stack_bytes").unwrap();
+        let phase1_memory_ts_rlc = phase1_idx_map.get("phase1_memory_ts_rlc").unwrap();
+
+        // assign witnesses to circuit
+        let phase0_witness_size = PushInstruction::<1>::witness_size(0);
+        let phase1_witness_size = PushInstruction::<1>::witness_size(1);
+
+        let n_wires_in = circuit.n_wires_in;
+        let mut wires_in = vec![vec![vec![]; n_wires_in]; 2];
+        wires_in[0][phase0_input_idx as usize] = vec![
+            Goldilocks::from(0u64); phase0_witness_size
+        ];
+        wires_in[1][phase1_input_idx as usize] = vec![
+            Goldilocks::from(0u64); phase1_witness_size
+        ];
+        
+        let phase0_pc_values = vec![
+            Goldilocks::from(1u64),
+        ];
+        let phase0_stack_ts_values = vec![
+            Goldilocks::from(1u64),
+        ];
+        let phase0_stack_top = vec![
+            Goldilocks::from(1u64),
+        ];
+        let phase0_clk = vec![
+            Goldilocks::from(1u64),
+        ]; 
+        let phase0_pc_add_i_plus_1 = vec![
+            Goldilocks::from(1u64),
+            Goldilocks::from(1u64),
+        ];
+        let phase0_stack_ts_add = vec![
+            Goldilocks::from(2u64),
+        ];
+        let phase0_stack_bytes = vec![
+            Goldilocks::from(0u64),
+        ];
+        let phase1_memory_ts_rlc = vec![
+            Goldilocks::from(0u64),
+        ];
+
+        for (values_idx, wires_in_idx) in phase0_pc
+                                                        .clone()
+                                                        .collect::<Vec<_>>()
+                                                        .into_iter()
+                                                        .enumerate() {
+            if values_idx < phase0_pc_values.len() {
+                wires_in[0][phase0_input_idx as usize][wires_in_idx] = phase0_pc_values[values_idx];
+            }
+        }
+
+        /*
+        let circuit_witness = {
+            let challenge = Goldilocks::from(9);
+            let mut circuit_witness = CircuitWitness::new(&circuit, vec![challenge]);
+            for i in 0..2 {
+                circuit_witness.add_instance(&circuit, &wires_in[i]);
+            }
+            circuit_witness
+        };
+        */
+    }
+}
