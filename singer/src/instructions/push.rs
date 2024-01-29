6fe096be339cd7eee50ac18ff30fdc85c963cb60
huwenqing0606 <huwenqing.pku@gmail.com>
2024-01-29 14:50:58 -0600

    continue with building general test scheme for opcode using push as example. TODO: understand circuit's challenges input format and apply it

--- singer/src/instructions/push.rs
+++ singer/src/instructions/push.rs
@@ -253,91 +253,201 @@ mod test{
         }
     }
 
-
     #[test]
     fn test_push1_construct_circuit() {
         let chip_challenges = ChipChallenges::default();
-        
-        // configure a push1 circuit
-        let push1_InstCircuit 
+
+        // initialize general test inputs associated with push1       
+        let InstCircuit 
             = PushInstruction::<1>::construct_circuit::<Goldilocks>(chip_challenges).unwrap();
-        let circuit = push1_InstCircuit.circuit.as_ref();
+        let phase0_idx_map = PushInstruction::<1>::phase0_sizes();
+        let phase1_idx_map = PushInstruction::<1>::phase1_sizes();
+        let phase0_witness_size = PushInstruction::<1>::witness_size(0);
+        let phase1_witness_size = PushInstruction::<1>::witness_size(1);
+        let mut phase0_values_map = BTreeMap::<String, Vec<Goldilocks>>::new();
+        let mut phase1_values_map = BTreeMap::<String, Vec<Goldilocks>>::new();
+        phase0_values_map.insert(
+        "phase0_pc".to_string(), 
+        vec![
+            Goldilocks::from(1u64), 
+        ]);
+        phase0_values_map.insert(
+        "phase0_stack_ts".to_string(),
+        vec![
+            Goldilocks::from(1u64), 
+        ]);
+        phase0_values_map.insert(
+        "phase0_stack_top".to_string(),
+        vec![
+            Goldilocks::from(1u64), 
+        ]);
+        phase0_values_map.insert(
+        "phase0_clk".to_string(), 
+        vec![
+            Goldilocks::from(1u64),
+        ]);
+        phase0_values_map.insert(
+        "phase0_pc_add_i_plus_1".to_string(), 
+        vec![
+            Goldilocks::from(1u64),
+            Goldilocks::from(1u64),
+        ]);
+        phase0_values_map.insert(
+        "phase0_stack_ts_add".to_string(), 
+        vec![
+            Goldilocks::from(2u64),
+        ]);
+        phase0_values_map.insert(
+        "phase0_stack_bytes".to_string(), 
+        vec![
+            Goldilocks::from(0u64),
+        ]);
+        phase1_values_map.insert(
+        "phase1_memory_ts_rlc".to_string(), 
+        vec![
+            Goldilocks::from(0u64),
+        ]);
+
+        // TODO: Refactor general opcode test logic below 
+        // configure circuit
+        let circuit = InstCircuit.circuit.as_ref();
         
         // get indexes for circuit inputs and wire_in
         // they are divided into phase0 and phase1
-        let inputs_idxes = &push1_InstCircuit.layout.phases_wire_id;
+        let inputs_idxes = &InstCircuit.layout.phases_wire_id;
         let phase0_input_idx = inputs_idxes[0].unwrap();
         let phase1_input_idx = inputs_idxes[1].unwrap();
-
-        let phase0_idx_map = PushInstruction::<1>::phase0_sizes();
-        let phase1_idx_map = PushInstruction::<1>::phase1_sizes();
-        let phase0_pc = phase0_idx_map.get("phase0_pc").unwrap();
-        let phase0_stack_ts = phase0_idx_map.get("phase0_stack_ts").unwrap();
-        let phase0_stack_top = phase0_idx_map.get("phase0_stack_top").unwrap();
-        let phase0_clk = phase0_idx_map.get("phase0_clk").unwrap();
-        let phase0_pc_add_i_plus_1 = phase0_idx_map.get("phase0_pc_add_i_plus_1").unwrap();
-        let phase0_stack_ts_add = phase0_idx_map.get("phase0_stack_ts_add").unwrap();
-        let phase0_stack_bytes = phase0_idx_map.get("phase0_stack_bytes").unwrap();
-        let phase1_memory_ts_rlc = phase1_idx_map.get("phase1_memory_ts_rlc").unwrap();
-
+        
         // assign witnesses to circuit
-        let phase0_witness_size = PushInstruction::<1>::witness_size(0);
-        let phase1_witness_size = PushInstruction::<1>::witness_size(1);
-
         let n_wires_in = circuit.n_wires_in;
-        let mut wires_in = vec![vec![vec![]; n_wires_in]; 2];
-        wires_in[0][phase0_input_idx as usize] = vec![
+        let mut wires_in = vec![vec![]; n_wires_in];
+        wires_in[phase0_input_idx as usize] = vec![
             Goldilocks::from(0u64); phase0_witness_size
         ];
-        wires_in[1][phase1_input_idx as usize] = vec![
+        wires_in[phase1_input_idx as usize] = vec![
             Goldilocks::from(0u64); phase1_witness_size
         ];
-        
-        let phase0_pc_values = vec![
-            Goldilocks::from(1u64),
-        ];
-        let phase0_stack_ts_values = vec![
-            Goldilocks::from(1u64),
-        ];
-        let phase0_stack_top = vec![
-            Goldilocks::from(1u64),
-        ];
-        let phase0_clk = vec![
-            Goldilocks::from(1u64),
-        ]; 
-        let phase0_pc_add_i_plus_1 = vec![
-            Goldilocks::from(1u64),
-            Goldilocks::from(1u64),
-        ];
-        let phase0_stack_ts_add = vec![
-            Goldilocks::from(2u64),
-        ];
-        let phase0_stack_bytes = vec![
-            Goldilocks::from(0u64),
-        ];
-        let phase1_memory_ts_rlc = vec![
-            Goldilocks::from(0u64),
-        ];
 
-        for (values_idx, wires_in_idx) in phase0_pc
-                                                        .clone()
-                                                        .collect::<Vec<_>>()
-                                                        .into_iter()
-                                                        .enumerate() {
-            if values_idx < phase0_pc_values.len() {
-                wires_in[0][phase0_input_idx as usize][wires_in_idx] = phase0_pc_values[values_idx];
+        for phase in 0..2 {
+            let idx_map = match phase {
+                0 => &phase0_idx_map,
+                1 => &phase1_idx_map,
+                other => panic!("invalid phase"),
+            };
+            let values_map = match phase {
+                0 => &phase0_values_map,
+                1 => &phase1_values_map,
+                other => panic!("invalid phase"),
+            };
+            let input_idx = match phase {
+                0 => phase0_input_idx as usize,
+                1 => phase1_input_idx as usize,
+                other => panic!("invalid phase"),
+            };
+            for key in idx_map.keys() {
+                let range = idx_map
+                                        .get(key)
+                                        .unwrap()
+                                        .clone()
+                                        .collect::<Vec<_>>();
+                let values = values_map
+                                            .get(key)
+                                            .unwrap();
+                for (value_idx, wire_in_idx) in range
+                                                            .into_iter()
+                                                            .enumerate() {
+                    if value_idx < values.len() {
+                        wires_in[input_idx as usize][wire_in_idx] 
+                            = values[value_idx];
+                    }
+                }
             }
         }
 
+        println!("{:?}", wires_in);    
+
         /*
         let circuit_witness = {
-            let challenge = Goldilocks::from(9);
-            let mut circuit_witness = CircuitWitness::new(&circuit, vec![challenge]);
-            for i in 0..2 {
-                circuit_witness.add_instance(&circuit, &wires_in[i]);
-            }
+            let challenges = vec![Goldilocks::from(2), Goldilocks::from(1)];
+            let mut circuit_witness = CircuitWitness::new(&circuit, challenges);
+            circuit_witness.add_instance(&circuit, &wires_in);
             circuit_witness
         };
+
+        println!("{:?}", circuit_witness);
+        */
+        /*
+        //#[cfg(feature = "debug")]
+        circuit_witness.check_correctness(&circuit);
+        */
+
+        /*
+        let instance_num_vars = circuit_witness.instance_num_vars();
+    
+        let (proof, output_num_vars, output_eval) = {
+            let mut prover_transcript = Transcript::<Goldilocks>::new(b"example");
+            let output_num_vars = instance_num_vars + circuit.last_layer_ref().num_vars();
+    
+            let output_point = (0..output_num_vars)
+                .map(|_| {
+                    prover_transcript
+                        .get_and_append_challenge(b"output point")
+                        .elements
+                })
+                .collect_vec();
+    
+            let output_eval = circuit_witness
+                .layer_poly(0, circuit.last_layer_ref().num_vars())
+                .evaluate(&output_point);
+            (
+                IOPProverState::prove_parallel(
+                    &circuit,
+                    &circuit_witness,
+                    &[(output_point, output_eval)],
+                    &[],
+                    &mut prover_transcript,
+                ),
+                output_num_vars,
+                output_eval,
+            )
+        };
+    
+        let gkr_input_claims = {
+            let mut verifier_transcript = &mut Transcript::<Goldilocks>::new(b"example");
+            let output_point = (0..output_num_vars)
+                .map(|_| {
+                    verifier_transcript
+                        .get_and_append_challenge(b"output point")
+                        .elements
+                })
+                .collect_vec();
+            IOPVerifierState::verify_parallel(
+                &circuit,
+                circuit_witness.challenges(),
+                &[(output_point, output_eval)],
+                &[],
+                &proof,
+                instance_num_vars,
+                &mut verifier_transcript,
+            )
+            .expect("verification failed")
+        };
+    
+        let expected_values = circuit_witness
+            .wires_in_ref()
+            .iter()
+            .map(|witness| {
+                witness
+                    .as_slice()
+                    .mle(circuit.max_wires_in_num_vars, instance_num_vars)
+                    .evaluate(&gkr_input_claims.point)
+            })
+            .collect_vec();
+        for i in 0..gkr_input_claims.values.len() {
+            assert_eq!(expected_values[i], gkr_input_claims.values[i]);
+        }
+    
+        println!("verification succeeded");
         */
     }
 }
