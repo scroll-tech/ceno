edf31430dda19b72249328bd72f547406e06aa96
huwenqing0606 <huwenqing.pku@gmail.com>
2024-04-10 15:33:21 -0500

    jumpi dbg result

--- singer/src/instructions/jumpi.rs
+++ singer/src/instructions/jumpi.rs
@@ -36,9 +36,9 @@ register_witness!(
         clk => 1,
 
         old_stack_ts_dest => TSUInt::N_OPRAND_CELLS,
-        old_stack_ts_dest_lt => UIntCmp::<TSUInt>::N_NO_OVERFLOW_WITNESS_CELLS,
+        old_stack_ts_dest_lt => UIntCmp::<TSUInt>::N_WITNESS_CELLS,
         old_stack_ts_cond => TSUInt::N_OPRAND_CELLS,
-        old_stack_ts_cond_lt => UIntCmp::<TSUInt>::N_NO_OVERFLOW_WITNESS_CELLS,
+        old_stack_ts_cond_lt => UIntCmp::<TSUInt>::N_WITNESS_CELLS,
 
         dest_values => StackUInt::N_OPRAND_CELLS,
         cond_values => StackUInt::N_OPRAND_CELLS,
@@ -185,5 +185,192 @@ impl<F: SmallField> Instruction<F> for JumpiInstruction {
                 ..Default::default()
             },
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use core::ops::Range;
+    use std::collections::BTreeMap;
+
+    use crate::instructions::{ChipChallenges, Instruction, JumpiInstruction};
+    use crate::test::{get_uint_params, test_opcode_circuit, u2vec};
+    use goldilocks::Goldilocks;
+    use simple_frontend::structs::CellId;
+    use singer_utils::constants::RANGE_CHIP_BIT_WIDTH;
+    use singer_utils::structs::TSUInt;
+
+    impl JumpiInstruction {
+        #[inline]
+        fn phase0_idxes_map() -> BTreeMap<String, Range<CellId>> {
+            let mut map = BTreeMap::new();
+            map.insert("phase0_pc".to_string(), Self::phase0_pc());
+            map.insert("phase0_stack_ts".to_string(), Self::phase0_stack_ts());
+            map.insert("phase0_memory_ts".to_string(), Self::phase0_memory_ts());
+            map.insert("phase0_stack_top".to_string(), Self::phase0_stack_top());
+            map.insert("phase0_clk".to_string(), Self::phase0_clk());
+            map.insert(
+                "phase0_old_stack_ts_dest".to_string(),
+                Self::phase0_old_stack_ts_dest(),
+            );
+            map.insert(
+                "phase0_old_stack_ts_dest_lt".to_string(),
+                Self::phase0_old_stack_ts_dest_lt(),
+            );
+            map.insert(
+                "phase0_old_stack_ts_cond".to_string(),
+                Self::phase0_old_stack_ts_cond(),
+            );
+            map.insert(
+                "phase0_old_stack_ts_cond_lt".to_string(),
+                Self::phase0_old_stack_ts_cond_lt(),
+            );
+            map.insert("phase0_dest_values".to_string(), Self::phase0_dest_values());
+            map.insert("phase0_cond_values".to_string(), Self::phase0_cond_values());
+            map.insert(
+                "phase0_cond_values_inv".to_string(),
+                Self::phase0_cond_values_inv(),
+            );
+            map.insert(
+                "phase0_cond_non_zero_or_inv".to_string(),
+                Self::phase0_cond_non_zero_or_inv(),
+            );
+            map.insert("phase0_pc_add".to_string(), Self::phase0_pc_add());
+            map.insert(
+                "phase0_pc_plus_1_opcode".to_string(),
+                Self::phase0_pc_plus_1_opcode(),
+            );
+
+            map
+        }
+    }
+
+    #[test]
+    fn test_jumpi_construct_circuit() {
+        let challenges = ChipChallenges::default();
+
+        let phase0_idx_map = JumpiInstruction::phase0_idxes_map();
+        let phase0_witness_size = JumpiInstruction::phase0_size();
+
+        #[cfg(feature = "witness-count")]
+        {
+            println!("JUMPI {:?}", &phase0_idx_map);
+            println!("JUMPI witness_size: {:?}", phase0_witness_size);
+        }
+
+        // initialize general test inputs associated with push1
+        let inst_circuit = JumpiInstruction::construct_circuit(challenges).unwrap();
+
+        #[cfg(feature = "test-dbg")]
+        println!("{:?}", inst_circuit);
+
+        let mut phase0_values_map = BTreeMap::<String, Vec<Goldilocks>>::new();
+        phase0_values_map.insert("phase0_pc".to_string(), vec![Goldilocks::from(1u64)]);
+        phase0_values_map.insert("phase0_stack_ts".to_string(), vec![Goldilocks::from(3u64)]);
+        phase0_values_map.insert("phase0_memory_ts".to_string(), vec![Goldilocks::from(1u64)]);
+        phase0_values_map.insert(
+            "phase0_stack_top".to_string(),
+            vec![Goldilocks::from(100u64)],
+        );
+        phase0_values_map.insert("phase0_clk".to_string(), vec![Goldilocks::from(1u64)]);
+        phase0_values_map.insert(
+            "phase0_old_stack_ts_dest".to_string(),
+            vec![Goldilocks::from(2u64)],
+        );
+        let m: u64 = (1 << get_uint_params::<TSUInt>().1) - 1;
+        let range_values = u2vec::<{ TSUInt::N_RANGE_CHECK_CELLS }, RANGE_CHIP_BIT_WIDTH>(m);
+        phase0_values_map.insert(
+            "phase0_old_stack_ts_dest_lt".to_string(),
+            vec![
+                Goldilocks::from(range_values[0]),
+                Goldilocks::from(range_values[1]),
+                Goldilocks::from(range_values[2]),
+                Goldilocks::from(range_values[3]),
+                Goldilocks::from(1u64), // borrow
+            ],
+        );
+        phase0_values_map.insert(
+            "phase0_old_stack_ts_cond".to_string(),
+            vec![Goldilocks::from(1u64)],
+        );
+        let m: u64 = (1 << get_uint_params::<TSUInt>().1) - 2;
+        let range_values = u2vec::<{ TSUInt::N_RANGE_CHECK_CELLS }, RANGE_CHIP_BIT_WIDTH>(m);
+        phase0_values_map.insert(
+            "phase0_old_stack_ts_cond_lt".to_string(),
+            vec![
+                Goldilocks::from(range_values[0]),
+                Goldilocks::from(range_values[1]),
+                Goldilocks::from(range_values[2]),
+                Goldilocks::from(range_values[3]),
+                Goldilocks::from(1u64), // borrow
+            ],
+        );
+        phase0_values_map.insert(
+            "phase0_dest_values".to_string(),
+            vec![
+                Goldilocks::from(7u64),
+                Goldilocks::from(6u64),
+                Goldilocks::from(5u64),
+                Goldilocks::from(4u64),
+                Goldilocks::from(3u64),
+                Goldilocks::from(2u64),
+                Goldilocks::from(1u64),
+                Goldilocks::from(0u64),
+            ],
+        );
+        phase0_values_map.insert(
+            "phase0_cond_values".to_string(),
+            vec![
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+            ], // when cond is zero, pc is increased by 1
+        );
+        phase0_values_map.insert(
+            "phase0_cond_values_inv".to_string(),
+            vec![
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+                Goldilocks::from(0u64),
+            ], 
+        );
+        phase0_values_map.insert(
+            "phase0_cond_non_zero_or_inv".to_string(),
+            vec![Goldilocks::from(0u64)],
+        );
+        phase0_values_map.insert(
+            "phase0_pc_add".to_string(),
+            vec![], // carry is 0, may test carry using larger values in PCUInt
+        );
+        phase0_values_map.insert(
+            "phase0_pc_plus_1_opcode".to_string(),
+            vec![Goldilocks::from(2u64)],
+        );
+
+        println!("phase0_values_map {:?}", phase0_values_map);
+        
+        let circuit_witness_challenges = vec![
+            Goldilocks::from(2),
+            Goldilocks::from(2),
+            Goldilocks::from(2),
+        ];
+
+        let _circuit_witness = test_opcode_circuit(
+            &inst_circuit,
+            &phase0_idx_map,
+            phase0_witness_size,
+            &phase0_values_map,
+            circuit_witness_challenges,
+        );
     }
 }
