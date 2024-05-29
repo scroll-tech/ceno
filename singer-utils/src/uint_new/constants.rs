9bd42ed74e5e8635fcf10e53b49b20bdb6138a6e
Wisdom Ogwu <iammadab@gmail.com>
2024-05-29 16:04:40 +0100

    document N_OPERAND_CELLS constant

--- singer-utils/src/uint_new/constants.rs
+++ singer-utils/src/uint_new/constants.rs
@@ -1,8 +1,7 @@
 use super::uint::UInt;
 
-// TODO: determine constant access controls
-
 impl<const M: usize, const C: usize> UInt<M, C> {
-    // TODO: explain why this and how this
+    /// `N_OPERAND_CELLS` represent the minimum number of cells each of size `C` needed
+    /// to hold `M` total bits
     pub(crate) const N_OPERAND_CELLS: usize = (M + C - 1) / C;
 }
