63d86dffb05a564f468961dd2d881bd0e8d946a0
Wisdom Ogwu <iammadab@gmail.com>
2024-07-07 23:16:34 +0100

    add interior mutability to call data chip

--- singer-utils/src/chip_handler_new/calldata.rs
+++ singer-utils/src/chip_handler_new/calldata.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 use crate::chip_handler_new::rom_handler::ROMHandler;
 use crate::chip_handler_new::util::cell_to_mixed;
 use crate::structs::ROMType;
@@ -5,15 +7,14 @@ use ff_ext::ExtensionField;
 use itertools::Itertools;
 use simple_frontend::structs::{CellId, CircuitBuilder, MixedCell};
 
-// TODO: consider chip instantiation with the rom_handler then use later
-//  for all chips
-struct CalldataChip {}
+struct CalldataChip<Ext: ExtensionField> {
+    rom_handler: Rc<RefCell<ROMHandler<Ext>>>
+}
 
-impl CalldataChip {
+impl<Ext: ExtensionField> CalldataChip<Ext> {
     // TODO: rename and document
-    fn load<Ext: ExtensionField>(
-        &mut self,
-        rom_handler: &mut ROMHandler<Ext>,
+    fn load(
+        &self,
         circuit_builder: &mut CircuitBuilder<Ext>,
         offset: &[CellId],
         data: &[CellId],
@@ -26,6 +27,6 @@ impl CalldataChip {
         ]
         .concat();
         let data = data.iter().map(|&x| x.into()).collect_vec();
-        rom_handler.read_mixed(circuit_builder, &key, &data);
+        self.rom_handler.borrow_mut().read_mixed(circuit_builder, &key, &data);
     }
 }
