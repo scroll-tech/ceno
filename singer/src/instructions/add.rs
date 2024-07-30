47c75f6a15fd0225ad61fbbc467ec5f2356649ba
Wisdom Ogwu <iammadab@gmail.com>
2024-07-30 12:14:06 +0100

    fix add instruction

--- singer/src/instructions/add.rs
+++ singer/src/instructions/add.rs
@@ -6,7 +6,7 @@ use simple_frontend::structs::{CircuitBuilder, MixedCell};
 use singer_utils::{
     chip_handler::{
         bytecode::BytecodeChip, global_state::GlobalStateChip, ram_handler::RAMHandler,
-        range::RangeChip, rom_handler::ROMHandler, stack::StackChip,
+        range::RangeChip, rom_handler::ROMHandler, stack::StackChip, ChipHandler,
     },
     constants::OpcodeType,
     register_witness,
@@ -55,14 +55,7 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
         let mut circuit_builder = CircuitBuilder::new();
         let (phase0_wire_id, phase0) = circuit_builder.create_witness_in(Self::phase0_size());
 
-        let mut rom_handler = Rc::new(RefCell::new(ROMHandler::new(challenges.clone())));
-        let mut ram_handler = Rc::new(RefCell::new(RAMHandler::new(challenges.clone())));
-
-        // instantiate chips
-        let global_state_chip = GlobalStateChip::new(ram_handler.clone());
-        let mut range_chip = RangeChip::new(rom_handler.clone());
-        let stack_chip = StackChip::new(ram_handler.clone());
-        let bytecode_chip = BytecodeChip::new(rom_handler.clone());
+        let mut chip_handler = ChipHandler::new(challenges.clone());
 
         // State update
         let pc = PCUInt::try_from(&phase0[Self::phase0_pc()])?;
@@ -72,7 +65,8 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
         let stack_top_expr = MixedCell::Cell(stack_top);
         let clk = phase0[Self::phase0_clk().start];
         let clk_expr = MixedCell::Cell(clk);
-        global_state_chip.state_in(
+        GlobalStateChip::state_in(
+            &mut chip_handler,
             &mut circuit_builder,
             pc.values(),
             stack_ts.values(),
@@ -83,14 +77,16 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
 
         let next_pc =
             RangeChip::add_pc_const(&mut circuit_builder, &pc, 1, &phase0[Self::phase0_pc_add()])?;
-        let next_stack_ts = range_chip.add_ts_with_const(
+        let next_stack_ts = RangeChip::add_ts_with_const(
+            &mut chip_handler,
             &mut circuit_builder,
             &stack_ts,
             1,
             &phase0[Self::phase0_stack_ts_add()],
         )?;
 
-        global_state_chip.state_out(
+        GlobalStateChip::state_out(
+            &mut chip_handler,
             &mut circuit_builder,
             next_pc.values(),
             next_stack_ts.values(),
@@ -109,14 +105,15 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
         );
         let result = StackUInt::add(
             &mut circuit_builder,
-            &mut range_chip,
+            &mut chip_handler,
             &addend_0,
             &addend_1,
             &phase0[Self::phase0_instruction_add()],
         )?;
 
         // Check the range of stack_top - 2 is within [0, 1 << STACK_TOP_BIT_WIDTH).
-        range_chip.range_check_stack_top(
+        RangeChip::range_check_stack_top(
+            &mut chip_handler,
             &mut circuit_builder,
             stack_top_expr.sub(E::BaseField::from(2)),
         )?;
@@ -125,12 +122,13 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
         let old_stack_ts0 = (&phase0[Self::phase0_old_stack_ts0()]).try_into()?;
         TSUInt::assert_lt(
             &mut circuit_builder,
-            &mut range_chip,
+            &mut chip_handler,
             &old_stack_ts0,
             &stack_ts,
             &phase0[Self::phase0_old_stack_ts_lt0()],
         )?;
-        stack_chip.pop(
+        StackChip::pop(
+            &mut chip_handler,
             &mut circuit_builder,
             stack_top_expr.sub(E::BaseField::from(1)),
             old_stack_ts0.values(),
@@ -140,12 +138,13 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
         let old_stack_ts1 = (&phase0[Self::phase0_old_stack_ts1()]).try_into()?;
         TSUInt::assert_lt(
             &mut circuit_builder,
-            &mut range_chip,
+            &mut chip_handler,
             &old_stack_ts1,
             &stack_ts,
             &phase0[Self::phase0_old_stack_ts_lt1()],
         )?;
-        stack_chip.pop(
+        StackChip::pop(
+            &mut chip_handler,
             &mut circuit_builder,
             stack_top_expr.sub(E::BaseField::from(2)),
             &old_stack_ts1.values(),
@@ -153,7 +152,8 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
         );
 
         // Push one result to stack
-        stack_chip.push(
+        StackChip::push(
+            &mut chip_handler,
             &mut circuit_builder,
             stack_top_expr.sub(E::BaseField::from(2)),
             stack_ts.values(),
@@ -161,14 +161,14 @@ impl<E: ExtensionField> Instruction<E> for AddInstruction {
         );
 
         // Bytecode check for (pc, add)
-        bytecode_chip.bytecode_with_pc_opcode(
+        BytecodeChip::bytecode_with_pc_opcode(
+            &mut chip_handler,
             &mut circuit_builder,
             pc.values(),
             <Self as Instruction<E>>::OPCODE,
         );
 
-        let (ram_load_id, ram_store_id) = ram_handler.borrow_mut().finalize(&mut circuit_builder);
-        let rom_id = rom_handler.borrow_mut().finalize(&mut circuit_builder);
+        let (ram_load_id, ram_store_id, rom_id) = chip_handler.finalize(&mut circuit_builder);
         circuit_builder.configure();
 
         let outputs_wire_id = [ram_load_id, ram_store_id, rom_id];
