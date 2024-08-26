b2f65892f08f5dec6c967c63c3073384c8ec8c81
Soham Zemse <22412996+zemse@users.noreply.github.com>
2024-08-26 16:36:14 +0530

    refactor namespace logic

--- ceno_zkvm/src/chip_handler/general.rs
+++ ceno_zkvm/src/chip_handler/general.rs
@@ -88,10 +88,10 @@ impl<'a, E: ExtensionField> CircuitBuilder<'a, E> {
         NR: Into<String>,
         N: FnOnce() -> NR,
     {
-        self.cs.ns.push_namespace("require_zero".to_string());
-        let result = self.cs.require_zero(name_fn, assert_zero_expr);
-        self.cs.ns.pop_namespace();
-        result
+        self.namespace(
+            || "require_zero",
+            |cb| cb.cs.require_zero(name_fn, assert_zero_expr),
+        )
     }
 
     pub fn require_equal<NR, N>(
@@ -104,10 +104,10 @@ impl<'a, E: ExtensionField> CircuitBuilder<'a, E> {
         NR: Into<String>,
         N: FnOnce() -> NR,
     {
-        self.cs.ns.push_namespace("require_equal".to_string());
-        let result = self.cs.require_zero(name_fn, target - rlc_record);
-        self.cs.ns.pop_namespace();
-        result
+        self.namespace(
+            || "require_equal",
+            |cb| cb.cs.require_zero(name_fn, target - rlc_record),
+        )
     }
 
     pub fn require_one<NR, N>(&mut self, name_fn: N, expr: Expression<E>) -> Result<(), ZKVMError>
@@ -115,10 +115,10 @@ impl<'a, E: ExtensionField> CircuitBuilder<'a, E> {
         NR: Into<String>,
         N: FnOnce() -> NR,
     {
-        self.cs.ns.push_namespace("require_one".to_string());
-        let result = self.require_zero(name_fn, Expression::from(1) - expr);
-        self.cs.ns.pop_namespace();
-        result
+        self.namespace(
+            || "require_one",
+            |cb| cb.cs.require_zero(name_fn, Expression::from(1) - expr),
+        )
     }
 
     pub(crate) fn assert_ux<NR, N, const C: usize>(
@@ -142,13 +142,17 @@ impl<'a, E: ExtensionField> CircuitBuilder<'a, E> {
         NR: Into<String>,
         N: FnOnce() -> NR,
     {
-        let items: Vec<Expression<E>> = vec![
-            Expression::Constant(E::BaseField::from(ROMType::U5 as u64)),
-            expr,
-        ];
-        let rlc_record = self.rlc_chip_record(items);
-        self.cs.lk_record(name_fn, rlc_record)?;
-        Ok(())
+        self.namespace(
+            || "assert_u5",
+            |cb| {
+                let items: Vec<Expression<E>> = vec![
+                    Expression::Constant(E::BaseField::from(ROMType::U5 as u64)),
+                    expr,
+                ];
+                let rlc_record = cb.rlc_chip_record(items);
+                cb.cs.lk_record(name_fn, rlc_record)
+            },
+        )
     }
 
     fn assert_u16<NR, N>(&mut self, name_fn: N, expr: Expression<E>) -> Result<(), ZKVMError>
