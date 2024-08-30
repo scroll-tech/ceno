5390280f4e68809c6baefa56de9f332cad1f7d88
Aurélien Nicolas <info@nau.re>
2024-08-30 16:51:45 +0200

    runtime: move to examples/

--- ceno_rt/examples/ceno_rt_mini.rs
+++ ceno_rt/examples/ceno_rt_mini.rs
@@ -5,19 +5,22 @@ use core::{arch::asm, panic::PanicInfo};
 #[panic_handler]
 #[inline(never)]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
+    halt(1)
 }
 
 fn halt(exit_code: u32) -> ! {
     unsafe {
-        asm!("mv a0, {}", in(reg) exit_code);
-        asm!("li t0, 0x0");
+        asm!(
+            "mv a0, {}",
+            "li t0, 0x0",
+            in(reg) exit_code,
+        );
         riscv::asm::ecall();
     }
-    unreachable!();
+    unreachable!()
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub fn _start() -> ! {
     halt(0)
 }
