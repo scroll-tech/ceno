27467e2e28cbb18fdcd7dcfb86355ed4c0fc1423
Aurélien Nicolas <info@nau.re>
2024-08-31 16:48:48 +0200

    runtime: example program using memory and stack

--- ceno_emul/tests/test_elf.rs
+++ ceno_emul/tests/test_elf.rs
@@ -3,19 +3,48 @@ use ceno_emul::{ByteAddr, Program, StepRecord, VMState, CENO_PLATFORM};
 
 #[test]
 fn test_ceno_rt_mini() -> Result<()> {
-    let mut ctx = VMState::new(CENO_PLATFORM);
+    let mut state = VMState::new(CENO_PLATFORM);
 
     // Load an ELF program in memory.
     let program_elf = include_bytes!("./data/ceno_rt_mini");
     let program = Program::load_elf(program_elf, u32::MAX)?;
     for (addr, word) in program.image.iter() {
         let addr = ByteAddr(*addr).waddr();
-        ctx.init_memory(addr, *word);
+        state.init_memory(addr, *word);
     }
     assert_eq!(program.entry, CENO_PLATFORM.pc_start());
 
-    let _steps = run(&mut ctx)?;
-    let ctx = ctx;
+    let _steps = run(&mut state)?;
+
+    Ok(())
+}
+
+#[test]
+fn test_ceno_rt_mem() -> Result<()> {
+    let mut state = VMState::new(CENO_PLATFORM);
+
+    // Load an ELF program in memory.
+    let program_elf = include_bytes!("./data/ceno_rt_mem");
+    let program = Program::load_elf(program_elf, u32::MAX)?;
+    for (addr, word) in program.image.iter() {
+        let addr = ByteAddr(*addr).waddr();
+        state.init_memory(addr, *word);
+    }
+    assert_eq!(program.entry, CENO_PLATFORM.pc_start());
+
+    let mut prev_step = StepRecord::default();
+    for step in state.iter_until_success() {
+        match step {
+            Ok(step) => {
+                println!("{:?}", step);
+                prev_step = step;
+            }
+            Err(e) => {
+                println!("pc = {:?}", prev_step.pc().after);
+                return Err(e);
+            }
+        }
+    }
 
     Ok(())
 }
---
+++ ceno_rt/examples/ceno_rt_mem.rs
@@ -0,0 +1,46 @@
+#![no_main]
+#![no_std]
+use core::{arch::asm, panic::PanicInfo, ptr::addr_of_mut};
+
+#[panic_handler]
+#[inline(never)]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    halt(1)
+}
+
+fn halt(exit_code: u32) -> ! {
+    unsafe {
+        asm!(
+            "mv a0, {}",
+            "li t0, 0x0",
+            in(reg) exit_code,
+        );
+        riscv::asm::ecall();
+    }
+    unreachable!()
+}
+
+static mut OUTPUT: u32 = 0;
+
+#[inline(never)]
+fn output(out: u32) {
+    // Volatile write to prevent the compiler from optimizing this away.
+    unsafe { core::ptr::write_volatile(addr_of_mut!(OUTPUT), out) };
+}
+
+#[no_mangle]
+pub fn _start() -> ! {
+    let y = my_recurse(3, 0);
+    output(y);
+    halt(0)
+}
+
+#[inline(never)]
+#[no_mangle]
+fn my_recurse(x: u32, y: u32) -> u32 {
+    if x == 0 {
+        y
+    } else {
+        my_recurse(x - 1, y * 3 + 5)
+    }
+}
