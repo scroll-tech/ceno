b18988934e81b99d0a36a4c5d562ca7a521d7ff7
xkx <xiakunxian130@gmail.com>
2024-09-03 14:58:38 +0800

    add range table circuit (#154)
    Fixes #109.
    
    - [x] add range circuit and unit test
    - [x] add prover for table circuits: `create_table_proof`.
    - [x] add verifier for table circuits: `verify_table_proof`.

--- ceno_zkvm/src/lib.rs
+++ ceno_zkvm/src/lib.rs
@@ -4,7 +4,6 @@ pub mod error;
 pub mod instructions;
 pub mod scheme;
 pub mod tables;
-// #[cfg(test)]
 pub use utils::u64vec;
 mod chip_handler;
 pub mod circuit_builder;
@@ -13,3 +12,4 @@ mod structs;
 mod uint;
 mod utils;
 mod virtual_polys;
+mod witness;
