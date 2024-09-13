e376e15caf796c3a761d90b1aece28127f0c8222
KimiWu <leonhartwu@gmail.com>
2024-09-13 14:55:28 +0800

    add sanity check in test

--- ceno_zkvm/src/uint/arithmetic.rs
+++ ceno_zkvm/src/uint/arithmetic.rs
@@ -721,6 +721,8 @@ mod tests {
             witness_values: Vec<u64>,
             overflow: bool,
         ) {
+            let pow_of_c: u64 = 2_usize.pow(UInt::<M, C, E>::MAX_CELL_BIT_WIDTH as u32) as u64;
+            let single_wit_size = UInt::<M, C, E>::NUM_CELLS;
             if overflow {
                 assert_eq!(
                     witness_values.len() % single_wit_size,
@@ -739,8 +741,6 @@ mod tests {
                 .mul(|| "uint_c", &mut cb, &mut uint_b, overflow)
                 .unwrap();
 
-            let pow_of_c: u64 = 2_usize.pow(UInt::<M, C, E>::MAX_CELL_BIT_WIDTH as u32) as u64;
-            let single_wit_size = UInt::<M, C, E>::NUM_CELLS;
             let wit_end_idx = if overflow {
                 4 * single_wit_size
             } else {
