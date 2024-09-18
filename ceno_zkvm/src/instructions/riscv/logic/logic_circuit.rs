f8004b27e4fd8600d80227436b039eb985050cbf
Aurélien Nicolas <info@nau.re>
2024-09-18 16:47:36 +0200

    logic-ops: reorganize types

--- ceno_zkvm/src/instructions/riscv/logic/logic_circuit.rs
+++ ceno_zkvm/src/instructions/riscv/logic/logic_circuit.rs
@@ -24,7 +24,43 @@ pub trait LogicOp {
     const ROM_TYPE: ROMType;
 }
 
-/// This config handles R-Instructions that represent registers values as 4 * u8.
+/// The Instruction circuit for a given LogicOp.
+pub struct LogicInstruction<E, I>(PhantomData<(E, I)>);
+
+impl<E: ExtensionField, I: LogicOp> Instruction<E> for LogicInstruction<E, I> {
+    type InstructionConfig = LogicConfig<E>;
+
+    fn name() -> String {
+        format!("{:?}", I::INST_KIND)
+    }
+
+    fn construct_circuit(cb: &mut CircuitBuilder<E>) -> Result<Self::InstructionConfig, ZKVMError> {
+        let config = LogicConfig::construct_circuit(cb, I::INST_KIND)?;
+
+        // Constrain the registers based on the given lookup table.
+        UInt8::logic(
+            cb,
+            I::ROM_TYPE,
+            &config.rs1_read,
+            &config.rs2_read,
+            &config.rd_written,
+        )?;
+
+        Ok(config)
+    }
+
+    fn assign_instance(
+        config: &Self::InstructionConfig,
+        instance: &mut [MaybeUninit<<E as ExtensionField>::BaseField>],
+        lk_multiplicity: &mut LkMultiplicity,
+        step: &StepRecord,
+    ) -> Result<(), ZKVMError> {
+        config.assign_instance(instance, lk_multiplicity, step)
+    }
+}
+
+/// This config implements R-Instructions that represent registers values as 4 * u8.
+/// Non-generic code shared by several circuits.
 #[derive(Debug)]
 pub struct LogicConfig<E: ExtensionField> {
     r_insn: RInstructionConfig<E>,
@@ -38,13 +74,10 @@ impl<E: ExtensionField> LogicConfig<E> {
     fn construct_circuit(
         cb: &mut CircuitBuilder<E>,
         insn_kind: InsnKind,
-        rom_type: ROMType,
     ) -> Result<Self, ZKVMError> {
         let rs1_read = UInt8::new_unchecked(|| "rs1_read", cb)?;
         let rs2_read = UInt8::new_unchecked(|| "rs2_read", cb)?;
         let rd_written = UInt8::new_unchecked(|| "rd_written", cb)?;
-
-        UInt8::logic(cb, rom_type, &rs1_read, &rs2_read, &rd_written)?;
 
         let r_insn = RInstructionConfig::<E>::construct_circuit(
             cb,
@@ -61,44 +94,24 @@ impl<E: ExtensionField> LogicConfig<E> {
             rd_written,
         })
     }
-}
-
-pub struct LogicInstruction<E, I>(PhantomData<(E, I)>);
-
-impl<E: ExtensionField, I: LogicOp> Instruction<E> for LogicInstruction<E, I> {
-    type InstructionConfig = LogicConfig<E>;
-
-    fn name() -> String {
-        format!("{:?}", I::INST_KIND)
-    }
-
-    fn construct_circuit(cb: &mut CircuitBuilder<E>) -> Result<Self::InstructionConfig, ZKVMError> {
-        LogicConfig::construct_circuit(cb, I::INST_KIND, I::ROM_TYPE)
-    }
 
     fn assign_instance(
-        config: &Self::InstructionConfig,
+        &self,
         instance: &mut [MaybeUninit<<E as ExtensionField>::BaseField>],
         lk_multiplicity: &mut LkMultiplicity,
         step: &StepRecord,
     ) -> Result<(), ZKVMError> {
-        config
-            .r_insn
+        self.r_insn
             .assign_instance(instance, lk_multiplicity, step)?;
 
         let rs1_read = Value::new_unchecked(step.rs1().unwrap().value);
-        config
-            .rs1_read
-            .assign_limbs(instance, rs1_read.u16_fields());
+        self.rs1_read.assign_limbs(instance, rs1_read.u16_fields());
 
         let rs2_read = Value::new_unchecked(step.rs2().unwrap().value);
-        config
-            .rs2_read
-            .assign_limbs(instance, rs2_read.u16_fields());
+        self.rs2_read.assign_limbs(instance, rs2_read.u16_fields());
 
         let rd_written = Value::new_unchecked(step.rd().unwrap().value.after);
-        config
-            .rd_written
+        self.rd_written
             .assign_limbs(instance, rd_written.u16_fields());
 
         Ok(())
