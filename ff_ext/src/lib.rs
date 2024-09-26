252ce3d9b47408740bef81f1b6a33434395772fb
sm.wu <hero78119@gmail.com>
2024-09-26 15:34:35 +0800

    call to_canonical_u64_vec from another impl trait

--- ff_ext/src/lib.rs
+++ ff_ext/src/lib.rs
@@ -33,17 +33,12 @@ pub trait ExtensionField:
     fn from_limbs(limbs: &[Self::BaseField]) -> Self;
 
     /// Convert a field elements to a u64 vector
-    fn to_canonical_u64_vec(&self) -> Vec<u64> {
-        self.as_bases()
-            .iter()
-            .map(|a| a.to_canonical_u64())
-            .collect::<Vec<u64>>()
-    }
+    fn to_canonical_u64_vec(&self) -> Vec<u64>;
 }
 
 mod impl_goldilocks {
     use crate::ExtensionField;
-    use goldilocks::{Goldilocks, GoldilocksExt2};
+    use goldilocks::{ExtensionField as GoldilocksEF, Goldilocks, GoldilocksExt2};
 
     impl ExtensionField for GoldilocksExt2 {
         const DEGREE: usize = 2;
@@ -62,6 +57,10 @@ mod impl_goldilocks {
         /// Convert limbs into self
         fn from_limbs(limbs: &[Self::BaseField]) -> Self {
             Self([limbs[0], limbs[1]])
+        }
+
+        fn to_canonical_u64_vec(&self) -> Vec<u64> {
+            <GoldilocksExt2 as GoldilocksEF>::to_canonical_u64_vec(self)
         }
     }
 }
