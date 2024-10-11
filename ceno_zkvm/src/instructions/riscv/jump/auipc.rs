fd36ae63a2e3c8f72f3312e051911157b26e88b5
Bryan Gillespie <bryan@inversed.tech>
2024-10-11 10:26:45 -0600

    Revise AUIPC implementation to handle wrapping addition more efficiently

--- ceno_zkvm/src/instructions/riscv/jump/auipc.rs
+++ ceno_zkvm/src/instructions/riscv/jump/auipc.rs
@@ -5,19 +5,20 @@ use ff_ext::ExtensionField;
 use crate::{
     circuit_builder::CircuitBuilder,
     error::ZKVMError,
-    expression::ToExpr,
+    expression::{ToExpr, WitIn},
     instructions::{
         riscv::{constants::UInt, u_insn::UInstructionConfig, RIVInstruction},
         Instruction,
     },
+    set_val,
     witness::LkMultiplicity,
     Value,
 };
 
 pub struct AuipcConfig<E: ExtensionField> {
     pub u_insn: UInstructionConfig<E>,
-    pub pc_uint: UInt<E>,
-    pub imm: UInt<E>,
+    pub imm: WitIn,
+    pub overflow_bit: WitIn,
     pub rd_written: UInt<E>,
 }
 
@@ -34,30 +35,33 @@ impl<E: ExtensionField, I: RIVInstruction> Instruction<E> for AuipcCircuit<E, I>
     fn construct_circuit(
         circuit_builder: &mut CircuitBuilder<E>,
     ) -> Result<AuipcConfig<E>, ZKVMError> {
-        let pc_uint = UInt::new(|| "pc_limbs", circuit_builder)?;
-        let imm = UInt::new(|| "imm", circuit_builder)?;
-        let rd_written = pc_uint.add(|| "pc_limbs + imm", circuit_builder, &imm, true)?;
+        let imm = circuit_builder.create_witin(|| "imm")?;
+        let rd_written = UInt::new(|| "rd_written", circuit_builder)?;
 
         let u_insn = UInstructionConfig::construct_circuit(
             circuit_builder,
             I::INST_KIND,
-            &imm.value(),
+            &imm.expr(),
             rd_written.register_expr(),
         )?;
 
-        // constrain pc UInt to equal UInstruction pc WitIn
-        // A separate UInt representation is needed here because the AUIPC
-        // instruction does mod 2^32 arithmetic on pc
+        let overflow_bit = circuit_builder.create_witin(|| "overflow_bit")?;
+        circuit_builder.assert_bit(|| "is_bit", overflow_bit.expr())?;
+
+        // assert: imm + pc = rd_written + overflow_bit * 2^32
+        // valid formulation of mod 2^32 arithmetic because:
+        // - imm and pc are constrained to 4 bytes by instruction table lookup
+        // - rd_written is constrained to 4 bytes by UInt checked limbs
         circuit_builder.require_equal(
-            || "auipc pc_limbs = pc",
-            pc_uint.value(),
-            u_insn.vm_state.pc.expr(),
+            || "imm+pc = rd_written+2^32*overflow",
+            imm.expr() + u_insn.vm_state.pc.expr(),
+            rd_written.value() + overflow_bit.expr() * (1u64 << 32).into(),
         )?;
 
         Ok(AuipcConfig {
             u_insn,
-            pc_uint,
             imm,
+            overflow_bit,
             rd_written,
         })
     }
@@ -68,19 +72,19 @@ impl<E: ExtensionField, I: RIVInstruction> Instruction<E> for AuipcCircuit<E, I>
         lk_multiplicity: &mut LkMultiplicity,
         step: &ceno_emul::StepRecord,
     ) -> Result<(), ZKVMError> {
-        let pc = Value::new(step.pc().before.0, lk_multiplicity);
-        let imm = Value::new(step.insn().imm_or_funct7(), lk_multiplicity);
+        let pc: u32 = step.pc().before.0;
+        let imm: u32 = step.insn().imm_or_funct7();
+        let (sum, overflow) = pc.overflowing_add(imm);
 
-        let sum_pc_imm = pc.add(&imm, lk_multiplicity, true);
+        set_val!(instance, config.imm, imm as u64);
+        set_val!(instance, config.overflow_bit, overflow as u64);
+
+        let sum_limbs = Value::new(sum, lk_multiplicity);
+        config.rd_written.assign_value(instance, sum_limbs);
 
         config
             .u_insn
             .assign_instance(instance, lk_multiplicity, step)?;
-
-        config.pc_uint.assign_value(instance, pc);
-        config.imm.assign_value(instance, imm);
-
-        config.rd_written.assign_add_outcome(instance, &sum_pc_imm);
 
         Ok(())
     }
