281c9254d762a0e994910c7c97d703e4f2392d71
Matthias Goergens <matthias.goergens@gmail.com>
2024-10-23 18:41:39 +0800

    Basics work

--- ceno_zkvm/src/expr.rs
+++ ceno_zkvm/src/expr.rs
@@ -1,11 +1,11 @@
-use std::ops::Add;
-
 use crate::{
     expression::{Fixed, Instance},
     structs::{ChallengeId, WitnessId},
 };
 use bumpalo::Bump;
 use ff_ext::ExtensionField;
+use goldilocks::SmallField;
+use std::ops::Add;
 
 /// Contains a reference to [`ExprTree`] that is managed by [`ExprBuilder`].
 #[derive(Clone, Copy, Debug)]
@@ -46,6 +46,16 @@ pub enum ExprTree<'a, E: ExtensionField> {
     Challenge(ChallengeId, usize, E, E), // (challenge_id, power, scalar, offset)
 }
 
+use ff::Field;
+
+impl<E: ExtensionField> ExprTree<'_, E> {
+    pub const ZERO: ExprTree<'static, E> = ExprTree::<'static, E>::Constant(E::BaseField::ZERO);
+    pub const ONE: ExprTree<'static, E> = ExprTree::<'static, E>::Constant(E::BaseField::ONE);
+    // pub fn add(&self, other: &Self) -> Self {
+    //     ExprTree::Sum(CompoundExpr(self), CompoundExpr(other))
+    // }
+}
+
 /// Expr Builder.  Contains a [`Bump`] memory arena that will allocate and
 /// store all the [`ExprTree`]s.
 #[derive(Debug, Default)]
@@ -64,6 +74,43 @@ impl<'a, E: ExtensionField> From<&'a mut ExprTree<'a, E>> for CompoundExpr<'a, E
         CompoundExpr(value)
     }
 }
+
+// Implement From trait for unsigned types of at most 64 bits
+macro_rules! impl_from_unsigned {
+    ($($t:ty),*) => {
+        $(
+            impl<F: SmallField, E: ExtensionField<BaseField = F>> From<$t> for Expr<'_, E> {
+                fn from(value: $t) -> Self {
+                    Expr::Constant(F::from(value as u64))
+                }
+            }
+        )*
+    };
+}
+impl_from_unsigned!(u8, u16, u32, u64, usize);
+
+// Implement From trait for u128 separately since it requires explicit reduction
+impl<F: SmallField, E: ExtensionField<BaseField = F>> From<u128> for Expr<'_, E> {
+    fn from(value: u128) -> Self {
+        let reduced = value.rem_euclid(F::MODULUS_U64 as u128) as u64;
+        Expr::Constant(F::from(reduced))
+    }
+}
+
+// Implement From trait for signed types
+macro_rules! impl_from_signed {
+    ($($t:ty),*) => {
+        $(
+            impl<F: SmallField, E: ExtensionField<BaseField = F>> From<$t> for Expr<'_, E> {
+                fn from(value: $t) -> Self {
+                    let reduced = (value as i128).rem_euclid(F::MODULUS_U64 as i128) as u64;
+                    Expr::Constant(F::from(reduced))
+                }
+            }
+        )*
+    };
+}
+impl_from_signed!(i8, i16, i32, i64, i128, isize);
 
 impl ExprBuilder {
     /// Internalise an [`ExprTree`] by moving it to memory allocated by the
@@ -105,7 +152,7 @@ impl ExprBuilder {
     // }
 
     /// Allocate Constant Expr Tree in the Expr Builder
-    fn constant_tree<E: ExtensionField>(&self, constant: E::BaseField ) -> CompoundExpr<'_, E> {
+    fn constant_tree<E: ExtensionField>(&self, constant: E::BaseField) -> CompoundExpr<'_, E> {
         self.intern(ExprTree::Constant(constant))
     }
 
@@ -122,100 +169,91 @@ impl ExprBuilder {
 
 impl<'a, E: ExtensionField> Add for Expr<'a, E> {
     type Output = Self;
-    fn add(self, rhs: Expr<E>) -> Expr<E> {
+    fn add(self, rhs: Expr<'a, E>) -> Expr<'a, E> {
         let builder = match (self, rhs) {
             (Expr::Constant(c), Expr::Constant(d)) => return Expr::Constant(c + d),
             (Expr::Compound { builder, .. }, _) | (_, Expr::Compound { builder, .. }) => builder,
         };
-        todo!()
-        // match (&self, &rhs) {
-        //     // constant + witness
-        //     // constant + fixed
-        //     // constant + instance
-        //     (Expr::WitIn(_), Expr::Constant(_))
-        //     | (Expr::Fixed(_), Expr::Constant(_))
-        //     | (Expr::Instance(_), Expr::Constant(_)) => Expr::ScaledSum(
-        //         Box::new(self),
-        //         Box::new(Expr::Constant(E::BaseField::ONE)),
-        //         Box::new(rhs),
-        //     ),
-        //     (Expr::Constant(_), Expr::WitIn(_))
-        //     | (Expr::Constant(_), Expr::Fixed(_))
-        //     | (Expr::Constant(_), Expr::Instance(_)) => Expr::ScaledSum(
-        //         Box::new(rhs),
-        //         Box::new(Expr::Constant(E::BaseField::ONE)),
-        //         Box::new(self),
-        //     ),
-        //     // challenge + witness
-        //     // challenge + fixed
-        //     // challenge + instance
-        //     (Expr::WitIn(_), Expr::Challenge(..))
-        //     | (Expr::Fixed(_), Expr::Challenge(..))
-        //     | (Expr::Instance(_), Expr::Challenge(..)) => Expr::ScaledSum(
-        //         Box::new(self),
-        //         Box::new(Expr::Constant(E::BaseField::ONE)),
-        //         Box::new(rhs),
-        //     ),
-        //     (Expr::Challenge(..), Expr::WitIn(_))
-        //     | (Expr::Challenge(..), Expr::Fixed(_))
-        //     | (Expr::Challenge(..), Expr::Instance(_)) => Expr::ScaledSum(
-        //         Box::new(rhs),
-        //         Box::new(Expr::Constant(E::BaseField::ONE)),
-        //         Box::new(self),
-        //     ),
-        //     // constant + challenge
-        //     (
-        //         Expr::Constant(c1),
-        //         Expr::Challenge(challenge_id, pow, scalar, offset),
-        //     )
-        //     | (
-        //         Expr::Challenge(challenge_id, pow, scalar, offset),
-        //         Expr::Constant(c1),
-        //     ) => Expr::Challenge(*challenge_id, *pow, *scalar, *offset + c1),
+        let lhs = builder.ensure_interned(self).0;
+        let rhs = builder.ensure_interned(rhs).0;
 
-        //     // challenge + challenge
-        //     (
-        //         Expr::Challenge(challenge_id1, pow1, scalar1, offset1),
-        //         Expr::Challenge(challenge_id2, pow2, scalar2, offset2),
-        //     ) => {
-        //         if challenge_id1 == challenge_id2 && pow1 == pow2 {
-        //             Expr::Challenge(
-        //                 *challenge_id1,
-        //                 *pow1,
-        //                 *scalar1 + scalar2,
-        //                 *offset1 + offset2,
-        //             )
-        //         } else {
-        //             Expr::Sum(Box::new(self), Box::new(rhs))
-        //         }
-        //     }
+        // TODO(Matthias): perhaps move this to a method on ExprTree or so?
+        let one = || builder.ensure_interned(Self::from(1));
+        let result = match (lhs, rhs) {
+            // constant + witness
+            // constant + fixed
+            // constant + instance
+            (ExprTree::WitIn(_), ExprTree::Constant(_))
+            | (ExprTree::Fixed(_), ExprTree::Constant(_))
+            | (ExprTree::Instance(_), ExprTree::Constant(_)) => {
+                ExprTree::ScaledSum(CompoundExpr(lhs), one(), CompoundExpr(rhs))
+            }
+            (ExprTree::Constant(_), ExprTree::WitIn(_))
+            | (ExprTree::Constant(_), ExprTree::Fixed(_))
+            | (ExprTree::Constant(_), ExprTree::Instance(_)) => {
+                ExprTree::ScaledSum(CompoundExpr(rhs), one(), CompoundExpr(lhs))
+            }
+            // challenge + witness
+            // challenge + fixed
+            // challenge + instance
+            (ExprTree::WitIn(_), ExprTree::Challenge(..))
+            | (ExprTree::Fixed(_), ExprTree::Challenge(..))
+            | (ExprTree::Instance(_), ExprTree::Challenge(..)) => {
+                ExprTree::ScaledSum(CompoundExpr(lhs), one(), CompoundExpr(rhs))
+            }
+            (ExprTree::Challenge(..), ExprTree::WitIn(_))
+            | (ExprTree::Challenge(..), ExprTree::Fixed(_))
+            | (ExprTree::Challenge(..), ExprTree::Instance(_)) => {
+                ExprTree::ScaledSum(CompoundExpr(rhs), one(), CompoundExpr(lhs))
+            }
+            // constant + challenge
+            (ExprTree::Constant(c1), ExprTree::Challenge(challenge_id, pow, scalar, offset))
+            | (ExprTree::Challenge(challenge_id, pow, scalar, offset), ExprTree::Constant(c1)) => {
+                ExprTree::Challenge(*challenge_id, *pow, *scalar, *offset + c1)
+            }
 
-        //     // constant + constant
-        //     (Expr::Constant(c1), Expr::Constant(c2)) => Expr::Constant(*c1 + c2),
+            // challenge + challenge
+            (
+                ExprTree::Challenge(challenge_id1, pow1, scalar1, offset1),
+                ExprTree::Challenge(challenge_id2, pow2, scalar2, offset2),
+            ) if challenge_id1 == challenge_id2 && pow1 == pow2 => ExprTree::Challenge(
+                *challenge_id1,
+                *pow1,
+                *scalar1 + scalar2,
+                *offset1 + offset2,
+            ),
 
-        //     // constant + scaled sum
-        //     (c1 @ Expr::Constant(_), Expr::ScaledSum(x, a, b))
-        //     | (Expr::ScaledSum(x, a, b), c1 @ Expr::Constant(_)) => {
-        //         Expr::ScaledSum(
-        //             x.clone(),
-        //             a.clone(),
-        //             Box::new(b.deref().clone() + c1.clone()),
-        //         )
-        //     }
+            // constant + constant
+            (ExprTree::Constant(c1), ExprTree::Constant(c2)) => ExprTree::Constant(*c1 + c2),
 
-        //     _ => Expr::Sum(Box::new(self), Box::new(rhs)),
-        }
+            // constant + scaled sum
+            (ExprTree::Constant(c), ExprTree::ScaledSum(x, a, b))
+            | (ExprTree::ScaledSum(x, a, b), ExprTree::Constant(c)) => ExprTree::ScaledSum(
+                *x,
+                *a,
+                builder.ensure_interned(builder.wrap(*b) + Expr::Constant(*c)),
+            ),
+
+            _ => ExprTree::Sum(CompoundExpr(lhs), CompoundExpr(rhs)),
+        };
+        builder.wrap(builder.intern(result))
+    }
 }
 
 #[test]
 fn test_expr() {
-    use goldilocks::Goldilocks;
-    use goldilocks::GoldilocksExt2;
+    use goldilocks::{Goldilocks, GoldilocksExt2};
     type E = GoldilocksExt2;
+
+    let a = Expr::<'_, E>::from(1);
+    let b = Expr::<'_, E>::from(2);
+    let c = a + b;
+    println!("{:?}", c);
+
     let builder = ExprBuilder::default();
     let expr: CompoundExpr<'_, E> = builder.constant_tree(Goldilocks::from(1));
     let a = builder.wrap(expr);
     println!("{:?}", a);
-    // let b = a + a;
-    // println!("{:?}", b);
+    let b = a + c;
+    println!("{:?}", b);
 }
