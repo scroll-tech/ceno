b1568d375f36e81377b50dde15098397257a00a6
Mihai <mihai.calancea@gmail.com>
2024-11-08 15:09:32 +0200

    add comment about handling of spans for thread spawns

--- sumcheck/src/prover_v2.rs
+++ sumcheck/src/prover_v2.rs
@@ -91,6 +91,8 @@ impl<'a, E: ExtensionField> IOPProverStateV2<'a, E> {
                 let tx_prover_state = tx_prover_state.clone();
                 let mut thread_based_transcript = thread_based_transcript.clone();
                 let current_span = tracing::Span::current();
+                // NOTE: Apply the span.in_scope(||) pattern to record work of spawned thread inside
+                // span of parent thread.
                 s.spawn(move |_| {
                     current_span.in_scope(||{
                     let mut challenge = None;
