dd2eeb4f308dc6e8076cf5d5a9614ebfa8a3c015
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-03 12:23:17 +0800

    Simpler

--- sumcheck/src/macros.rs
+++ sumcheck/src/macros.rs
@@ -1,20 +1,14 @@
 use tracing::span;
 
 pub macro entered_span {
-    ($first:expr, $($fields:tt)*) => {
-        tracing_span!($first, $($fields)*).entered()
-    },
-    ($first:expr $(,)*) => {
-        tracing_span!($first).entered()
-    },
+    ($first:expr $(, $($fields:tt)*)?) => {
+        tracing_span!($first, $($($fields)*)?).entered()
+    }
 }
 
 pub macro tracing_span {
-    ($first:expr, $($fields:tt)*) => {
-        span!(tracing::Level::INFO, $first, $($fields)*)
-    },
-    ($first:expr $(,)*) => {
-        span!(tracing::Level::INFO, $first)
+    ($first:expr $(, $($fields:tt)*)?) => {
+        span!(tracing::Level::INFO, $first, $($($fields)*)?)
     },
 }
 
