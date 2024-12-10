73b477900b7888f77e84e31621049c60b5a8b82a
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-10 16:47:08 +0800

    Simplify guest's memory allocator
    Extracted from https://github.com/scroll-tech/ceno/pull/631

--- ceno_rt/src/allocator.rs
+++ ceno_rt/src/allocator.rs
@@ -4,8 +4,18 @@
 use core::alloc::{GlobalAlloc, Layout};
 
 struct SimpleAllocator {
-    next_alloc: usize,
+    next_alloc: *mut u8,
 }
+
+extern "C" {
+    // The address of this variable is the start of the heap (growing upwards).
+    static mut _sheap: u8;
+}
+
+#[global_allocator]
+static mut HEAP: SimpleAllocator = SimpleAllocator {
+    next_alloc: &raw mut _sheap,
+};
 
 unsafe impl GlobalAlloc for SimpleAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
@@ -14,16 +24,14 @@ unsafe impl GlobalAlloc for SimpleAllocator {
 
         let align = layout.align();
         // `Layout` contract forbids making a `Layout` with align=0, or align not power of 2.
-        // So we can safely use subtraction and a mask to ensure alignment without worrying about UB.
-        let offset = heap_pos & (align - 1);
-        if offset != 0 {
-            heap_pos = heap_pos.strict_add(align.strict_sub(offset));
-        }
+        core::hint::assert_unchecked(align.is_power_of_two());
+        core::hint::assert_unchecked(align != 0);
+        heap_pos = heap_pos.add(heap_pos.align_offset(align));
 
-        let ptr = heap_pos as *mut u8;
+        let ptr = heap_pos;
         // Panic on overflow.  We don't want to wrap around, and overwrite stack etc.
         // (We could also return a null pointer, but only malicious programs would ever hit this.)
-        heap_pos = heap_pos.strict_add(layout.size());
+        heap_pos = heap_pos.add(layout.size());
 
         HEAP.next_alloc = heap_pos;
         ptr
@@ -35,15 +43,4 @@ unsafe impl GlobalAlloc for SimpleAllocator {
 
     /// Never deallocate.
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
-}
-
-#[global_allocator]
-// We initialize `next_alloc` to 0xFFFF_FFFF to indicate that the heap has not been initialized.
-// The value is chosen to make any premature allocation fail.
-static mut HEAP: SimpleAllocator = SimpleAllocator {
-    next_alloc: 0xFFFF_FFFF,
-};
-
-pub unsafe fn init_heap() {
-    HEAP.next_alloc = core::ptr::from_ref::<u8>(&crate::_sheap).cast::<u8>() as usize;
 }
