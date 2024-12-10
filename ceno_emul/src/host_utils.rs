5091d3dd7896e7ceb25f0d655eeb3afc0b513fbd
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-10 17:56:06 +0800

    Just for fun

--- ceno_emul/src/host_utils.rs
+++ ceno_emul/src/host_utils.rs
@@ -1,24 +1,24 @@
-use crate::{ByteAddr, EmuContext, VMState};
+use std::iter::from_fn;
+
+use crate::{ByteAddr, EmuContext, VMState, WordAddr};
 
 const WORD_SIZE: usize = 4;
-const INFO_OUT_ADDR: u32 = 0xC000_0000;
+const INFO_OUT_ADDR: WordAddr = ByteAddr(0xC000_0000).waddr();
 
 pub fn read_all_messages(state: &VMState) -> Vec<String> {
-    let mut all_messages = Vec::new();
-    let mut word_offset = 0;
-    loop {
-        let out = read_message(state, word_offset);
-        if out.is_empty() {
-            break;
+    let mut offset: WordAddr = WordAddr::from(0);
+    from_fn(move || match read_message(state, offset) {
+        out if out.is_empty() => None,
+        out => {
+            offset += out.len().div_ceil(WORD_SIZE) as u32 + 1;
+            Some(out)
         }
-        word_offset += out.len().div_ceil(WORD_SIZE) as u32 + 1;
-        all_messages.push(out);
-    }
-    all_messages
+    })
+    .collect()
 }
 
-fn read_message(state: &VMState, word_offset: u32) -> String {
-    let out_addr = ByteAddr(INFO_OUT_ADDR).waddr() + word_offset;
+fn read_message(state: &VMState, offset: WordAddr) -> String {
+    let out_addr = INFO_OUT_ADDR + offset;
     let byte_len = state.peek_memory(out_addr) as usize;
     let word_len_up = byte_len.div_ceil(4);
 
