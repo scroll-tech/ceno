76a3cb711ef8b43d66dbd9daa71ac2c65a0a756f
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-12 09:40:16 +0800

    Fix and generialise

--- ceno_zkvm/src/uint.rs
+++ ceno_zkvm/src/uint.rs
@@ -606,6 +606,24 @@ pub struct Value<'a, T: Into<u64> + From<u32> + Copy + Default> {
     pub limbs: Cow<'a, [u16]>,
 }
 
+impl<'a, T: Into<u64> + From<u32> + Copy + Default> From<&'a Value<'a, T>> for &'a [u16] {
+    fn from(v: &'a Value<'a, T>) -> Self {
+        v.as_u16_limbs()
+    }
+}
+
+impl<'a, T: Into<u64> + From<u32> + Copy + Default> From<&Value<'a, T>> for u64 {
+    fn from(v: &Value<'a, T>) -> Self {
+        v.as_u64()
+    }
+}
+
+impl<'a, T: Into<u64> + From<u32> + Copy + Default> From<&Value<'a, T>> for u32 {
+    fn from(v: &Value<'a, T>) -> Self {
+        v.as_u32()
+    }
+}
+
 // TODO generalize to support non 16 bit limbs
 // TODO optimize api with fixed size array
 impl<'a, T: Into<u64> + From<u32> + Copy + Default> Value<'a, T> {
@@ -616,10 +634,7 @@ impl<'a, T: Into<u64> + From<u32> + Copy + Default> Value<'a, T> {
     const LIMBS: usize = (Self::M + 15) / 16;
 
     pub fn new(val: T, lkm: &mut LkMultiplicity) -> Self {
-        let uint = Value::<T> {
-            val,
-            limbs: Cow::Owned(Self::split_to_u16(val)),
-        };
+        let uint = Self::new_unchecked(val);
         Self::assert_u16(&uint.limbs, lkm);
         uint
     }
