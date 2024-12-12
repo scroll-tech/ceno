02f082807f605aaec3894b0330d440da9f64400d
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-12 16:50:55 +0800

    Move as suggested

--- ceno_emul/src/lib.rs
+++ ceno_emul/src/lib.rs
@@ -12,12 +12,11 @@ mod vm_state;
 pub use vm_state::VMState;
 
 mod rv32im;
-pub use rv32im::{EmuContext, InsnCategory, InsnFormat, InsnKind, Instruction};
+pub use rv32im::{
+    EmuContext, InsnCategory, InsnFormat, InsnKind, Instruction, encode_rv32, encode_rv32u,
+};
 
 mod elf;
 pub use elf::Program;
-
-mod rv32im_encode;
-pub use rv32im_encode::{encode_rv32, encode_rv32u};
 
 pub mod disassemble;
--- ceno_emul/src/rv32im.rs
+++ ceno_emul/src/rv32im.rs
@@ -20,6 +20,34 @@ use strum_macros::{Display, EnumIter};
 
 use super::addr::{ByteAddr, RegIdx, WORD_SIZE, Word, WordAddr};
 
+/// Convenience function to create an `Instruction` with the given fields.
+///
+/// Pass 0 for unused fields.
+pub const fn encode_rv32(kind: InsnKind, rs1: u32, rs2: u32, rd: u32, imm: i32) -> Instruction {
+    Instruction {
+        kind,
+        rs1: rs1 as usize,
+        rs2: rs2 as usize,
+        rd: rd as usize,
+        imm,
+        raw: 0,
+    }
+}
+
+/// Convenience function to create an `Instruction` with the given fields.
+///
+/// Pass 0 for unused fields.
+pub const fn encode_rv32u(kind: InsnKind, rs1: u32, rs2: u32, rd: u32, imm: u32) -> Instruction {
+    Instruction {
+        kind,
+        rs1: rs1 as usize,
+        rs2: rs2 as usize,
+        rd: rd as usize,
+        imm: imm as i32,
+        raw: 0,
+    }
+}
+
 pub trait EmuContext {
     // Handle environment call
     fn ecall(&mut self) -> Result<bool>;
