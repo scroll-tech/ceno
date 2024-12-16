cd2428fd68ee5bb74e7eebf7cde9cb3cf7f01c4c
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-16 18:01:59 +0800

    Explain

--- ceno_host/tests/test_elf.rs
+++ ceno_host/tests/test_elf.rs
@@ -135,7 +135,7 @@ fn test_sorting() -> Result<()> {
     let mut rng = rand::thread_rng();
 
     // Provide some random numbers to sort.
-    hints.write(&(0..1000).map(|_| rng.gen::<u32>()).collect::<Vec<_>>())?;
+    hints.write(&(0..1_000).map(|_| rng.gen::<u32>()).collect::<Vec<_>>())?;
 
     let all_messages = ceno_host::run(CENO_PLATFORM, ceno_examples::sorting, &hints);
     for (i, msg) in enumerate(&all_messages) {
@@ -144,6 +144,11 @@ fn test_sorting() -> Result<()> {
     Ok(())
 }
 
+/// Sorting with hints
+///
+/// This is an example of using non-deterministic computation in the guest (ie hints) to
+/// sort faster.  For one million numbers of input, this takes 25_750_500 cycles to run.
+/// While `test_sorting` above takes around 302_674_458 cycles.  That's a 10x speedup!
 #[test]
 fn test_sorting_with_hints() -> Result<()> {
     use rand::Rng;
@@ -151,7 +156,7 @@ fn test_sorting_with_hints() -> Result<()> {
     let mut rng = rand::thread_rng();
 
     // Provide some random numbers to sort.
-    let nums = (0..1000).map(|_| rng.gen::<u32>()).collect::<Vec<_>>();
+    let nums = (0..1_000).map(|_| rng.gen::<u32>()).collect::<Vec<_>>();
     hints.write(&nums)?;
     // Provide both the sorted answer, and the places where you can find the original number.
     let (answer, places): (Vec<_>, Vec<_>) = enumerate(nums)
