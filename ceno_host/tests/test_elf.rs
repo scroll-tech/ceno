823ec5ee395d6142928bd7e5bf17e8bad2409520
Aurélien Nicolas <info@nau.re>
2024-12-16 12:27:53 +0100

    ecall-keccak-merge: put back test after merge

--- ceno_host/tests/test_elf.rs
+++ ceno_host/tests/test_elf.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
-use ceno_emul::{ByteAddr, CENO_PLATFORM, EmuContext, InsnKind, Platform, StepRecord, VMState};
-use itertools::{Itertools, izip};
+use ceno_emul::{
+    CENO_PLATFORM, EmuContext, InsnKind, Platform, Program, StepRecord, VMState, WORD_SIZE,
+    host_utils::read_all_messages,
+};
+use ceno_host::CenoStdin;
+use itertools::{Itertools, enumerate, izip};
+use std::{collections::HashSet, sync::Arc};
 use tiny_keccak::keccakf;
 
 #[test]
@@ -62,15 +67,36 @@ fn test_ceno_rt_alloc() -> Result<()> {
 #[test]
 fn test_ceno_rt_io() -> Result<()> {
     let program_elf = ceno_examples::ceno_rt_io;
-    let mut state = VMState::new_from_elf(CENO_PLATFORM, program_elf)?;
+    let program = Program::load_elf(program_elf, u32::MAX)?;
+    let platform = Platform {
+        prog_data: Some(program.image.keys().copied().collect::<HashSet<u32>>()),
+        ..CENO_PLATFORM
+    };
+    let mut state = VMState::new(platform, Arc::new(program));
     let _steps = run(&mut state)?;
 
     let all_messages = read_all_messages(&state);
     for msg in &all_messages {
-        print!("{}", String::from_utf8_lossy(msg));
+        print!("{msg}");
     }
-    assert_eq!(&all_messages[0], "📜📜📜 Hello, World!\n".as_bytes());
-    assert_eq!(&all_messages[1], "🌏🌍🌎\n".as_bytes());
+    assert_eq!(&all_messages[0], "📜📜📜 Hello, World!\n");
+    assert_eq!(&all_messages[1], "🌏🌍🌎\n");
+    Ok(())
+}
+
+#[test]
+fn test_hints() -> Result<()> {
+    let mut hints = CenoStdin::default();
+    hints.write(&true)?;
+    hints.write(&"This is my hint string.".to_string())?;
+    hints.write(&1997_u32)?;
+    hints.write(&1999_u32)?;
+
+    let all_messages = ceno_host::run(CENO_PLATFORM, ceno_examples::hints, &hints);
+    for (i, msg) in enumerate(&all_messages) {
+        println!("{i}: {msg}");
+    }
+    assert_eq!(all_messages[0], "3992003");
     Ok(())
 }
 
@@ -147,35 +173,4 @@ fn run(state: &mut VMState) -> Result<Vec<StepRecord>> {
     let steps = state.iter_until_halt().collect::<Result<Vec<_>>>()?;
     eprintln!("Emulator ran for {} steps.", steps.len());
     Ok(steps)
-}
-
-const WORD_SIZE: usize = 4;
-const INFO_OUT_ADDR: u32 = 0xC000_0000;
-
-fn read_all_messages(state: &VMState) -> Vec<Vec<u8>> {
-    let mut all_messages = Vec::new();
-    let mut word_offset = 0;
-    loop {
-        let out = read_message(state, word_offset);
-        if out.is_empty() {
-            break;
-        }
-        word_offset += out.len().div_ceil(WORD_SIZE) as u32 + 1;
-        all_messages.push(out);
-    }
-    all_messages
-}
-
-fn read_message(state: &VMState, word_offset: u32) -> Vec<u8> {
-    let out_addr = ByteAddr(INFO_OUT_ADDR).waddr() + word_offset;
-    let byte_len = state.peek_memory(out_addr);
-    let word_len_up = byte_len.div_ceil(4);
-
-    let mut info_out = Vec::with_capacity(WORD_SIZE * word_len_up as usize);
-    for i in 1..1 + word_len_up {
-        let value = state.peek_memory(out_addr + i);
-        info_out.extend_from_slice(&value.to_le_bytes());
-    }
-    info_out.truncate(byte_len as usize);
-    info_out
 }
