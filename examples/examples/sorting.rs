a8457056a72842b5db0b2e00ac054905b31882e7
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-16 15:57:50 +0800

    Use standard library

--- examples/examples/sorting.rs
+++ examples/examples/sorting.rs
@@ -5,7 +5,7 @@ use rkyv::vec::ArchivedVec;
 
 fn main() {
     let input: &ArchivedVec<u32> = ceno_rt::read();
-    let mut scratch = input.to_vec();
+    let mut scratch: Vec<u32> = input.to_vec();
     scratch.sort();
     // Print any output you feel like, eg the first element of the sorted vector:
     println!("{}", scratch[0]);
