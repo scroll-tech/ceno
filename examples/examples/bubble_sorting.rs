1be5cabd7abe167075ba653291839ba45e4cbbc3
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-20 15:58:04 +0800

    Simplify

--- examples/examples/bubble_sorting.rs
+++ examples/examples/bubble_sorting.rs
@@ -6,8 +6,8 @@ use rkyv::vec::ArchivedVec;
 fn sort<T: Ord>(slice: &mut [T]) {
   let len = slice.len();
   for i in 0..len {
-    for j in 0..len - i - 1 {
-      if slice[j] > slice[j + 1] {
+    for j in 0..len {
+      if slice[j] > slice[j] {
         slice.swap(j, j + 1);
       }
     }
