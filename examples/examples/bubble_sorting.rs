3b0796c83286062950f8f34a2bef87ed701d6596
Matthias Goergens <matthias.goergens@gmail.com>
2024-12-20 15:56:53 +0800

    Std!

--- examples/examples/bubble_sorting.rs
+++ examples/examples/bubble_sorting.rs
@@ -1,6 +1,3 @@
-#![no_main]
-#![no_std]
-
 extern crate ceno_rt;
 use ceno_rt::println;
 use core::fmt::Write;
@@ -17,7 +14,6 @@ fn sort<T: Ord>(slice: &mut [T]) {
   }
 }
 
-ceno_rt::entry!(main);
 fn main() {
     let input: &ArchivedVec<u32> = ceno_rt::read();
     let mut scratch = input.to_vec();
