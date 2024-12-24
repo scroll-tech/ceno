28cdb9fc857909411e1321242baf4bf38f5005b0
Yuncong Zhang <yczhangsjtu@gmail.com>
2024-12-24 10:51:45 +0800

    Fix poly transform bug

--- mpcs/src/whir/utils.rs
+++ mpcs/src/whir/utils.rs
@@ -2,11 +2,15 @@ use ff_ext::ExtensionField;
 use multilinear_extensions::mle::DenseMultilinearExtension;
 use whir::poly_utils::coeffs::CoefficientList;
 
+use crate::util::arithmetic::interpolate_field_type_over_boolean_hypercube;
+
 use super::ff_base::BaseFieldWrapper;
 
 pub fn poly2whir<E: ExtensionField>(
     poly: &DenseMultilinearExtension<E>,
 ) -> CoefficientList<BaseFieldWrapper<E>> {
+    let mut poly = poly.clone();
+    interpolate_field_type_over_boolean_hypercube(&mut poly.evaluations);
     match &poly.evaluations {
         multilinear_extensions::mle::FieldType::Ext(_coeffs) => {
             panic!("WHIR only supports committing to base field polys now")
