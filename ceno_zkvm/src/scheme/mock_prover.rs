8f4e9d0189f94401178f800683b163c9c75d19df
sm.wu <hero78119@gmail.com>
2024-12-27 10:48:35 +0800

    refactor to replace v1 by v2 and cleanup suffix

--- ceno_zkvm/src/scheme/mock_prover.rs
+++ ceno_zkvm/src/scheme/mock_prover.rs
@@ -23,7 +23,7 @@ use ff_ext::ExtensionField;
 use generic_static::StaticTypeMap;
 use goldilocks::SmallField;
 use itertools::{Itertools, enumerate, izip};
-use multilinear_extensions::{mle::IntoMLEs, virtual_poly_v2::ArcMultilinearExtension};
+use multilinear_extensions::{mle::IntoMLEs, virtual_poly::ArcMultilinearExtension};
 use rand::thread_rng;
 use std::{
     collections::{HashMap, HashSet},
