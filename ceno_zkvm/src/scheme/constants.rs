5bc10b26cb86347ca490f22f10c561fc17e4caa6
Matthias Goergens <matthias.goergens@gmail.com>
2025-01-06 21:04:11 +0800

    Support sproll proving

--- ceno_zkvm/src/scheme/constants.rs
+++ ceno_zkvm/src/scheme/constants.rs
@@ -5,4 +5,5 @@ pub(crate) const SEL_DEGREE: usize = 2;
 pub const NUM_FANIN: usize = 2;
 pub const NUM_FANIN_LOGUP: usize = 2;
 
-pub const MAX_NUM_VARIABLES: usize = 24;
+// TODO(Matthias): what are the trade-offs for this value?
+pub const MAX_NUM_VARIABLES: usize = 27;
