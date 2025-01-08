558bee066680bf800bc0fbf4678c72f28df1334d
Matthias Goergens <matthias.goergens@gmail.com>
2025-01-08 13:28:35 +0800

    Only log once

--- multilinear_extensions/src/util.rs
+++ multilinear_extensions/src/util.rs
@@ -37,14 +37,13 @@ fn prev_power_of_two(n: usize) -> usize {
 
 /// Largest power of two that fits the available rayon threads
 pub fn max_usable_threads() -> usize {
-    if cfg!(test) {
-        1
-    } else {
+    static MAX_USABLE_THREADS: std::sync::OnceLock<usize> = std::sync::OnceLock::new();
+    *MAX_USABLE_THREADS.get_or_init(|| {
         let n = rayon::current_num_threads();
         let threads = prev_power_of_two(n);
         if n != threads {
             tracing::warn!("thread size {n} is not power of 2, using {threads} threads instead.");
         }
         threads
-    }
+    })
 }
