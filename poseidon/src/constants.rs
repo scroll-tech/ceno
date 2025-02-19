a43bc514dca059a5c1558b35e4bb9f9dcc9b2d3d
sm.wu <hero78119@gmail.com>
2025-02-19 16:20:06 +0800

    clippy fix

--- poseidon/src/constants.rs
+++ poseidon/src/constants.rs
@@ -1,5 +1,1 @@
 pub(crate) const DIGEST_WIDTH: usize = 4;
-
-pub(crate) const SPONGE_RATE: usize = 8;
-pub(crate) const SPONGE_CAPACITY: usize = 4;
-pub const SPONGE_WIDTH: usize = SPONGE_RATE + SPONGE_CAPACITY;
--- poseidon/src/lib.rs
+++ poseidon/src/lib.rs
@@ -1,11 +1,7 @@
 #![deny(clippy::cargo)]
 extern crate core;
 
-pub(crate) mod constants;
-pub(crate) mod plonky2_goldilock_mds;
-pub use constants::SPONGE_WIDTH;
-#[cfg(test)]
-pub use plonky2_goldilock_mds::P2MdsMatrixGoldilocks;
 pub mod challenger;
+pub(crate) mod constants;
 pub mod digest;
 pub mod poseidon_hash;
