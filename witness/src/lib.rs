bbb76befbae0e9b5445d11579ca3fea7183adddf
sm.wu <hero78119@gmail.com>
2025-03-18 11:15:49 +0800

    rmm padding

--- witness/src/lib.rs
+++ witness/src/lib.rs
@@ -5,7 +5,7 @@ use p3::{
 };
 use rand::{Rng, distributions::Standard, prelude::Distribution};
 use rayon::{
-    iter::{IntoParallelIterator, ParallelIterator},
+    iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator},
     slice::ParallelSliceMut,
 };
 use std::{
@@ -35,6 +35,9 @@ pub enum InstancePaddingStrategy {
 #[derive(Clone)]
 pub struct RowMajorMatrix<T: Sized + Sync + Clone + Send + Copy> {
     inner: p3::matrix::dense::RowMajorMatrix<T>,
+    // num_row is the real instance BEFORE padding
+    num_rows: usize,
+    is_padded: bool,
     padding_strategy: InstancePaddingStrategy,
 }
 
@@ -43,14 +46,20 @@ impl<T: Sized + Sync + Clone + Send + Copy + Default + PrimeCharacteristicRing> 
     where
         Standard: Distribution<T>,
     {
+        debug_assert!(rows > 0);
+        let num_row_padded = next_pow2_instance_padding(rows);
         Self {
-            inner: p3::matrix::dense::RowMajorMatrix::rand(rng, rows, cols),
+            inner: p3::matrix::dense::RowMajorMatrix::rand(rng, num_row_padded, cols),
+            num_rows: rows,
+            is_padded: true,
             padding_strategy: InstancePaddingStrategy::Default,
         }
     }
     pub fn empty() -> Self {
         Self {
             inner: p3::matrix::dense::RowMajorMatrix::new(vec![], 0),
+            num_rows: 0,
+            is_padded: true,
             padding_strategy: InstancePaddingStrategy::Default,
         }
     }
@@ -67,26 +76,40 @@ impl<T: Sized + Sync + Clone + Send + Copy + Default + PrimeCharacteristicRing> 
     }
 
     pub fn num_vars(&self) -> usize {
-        (next_pow2_instance_padding(self.num_instances())).ilog2() as usize
+        self.inner.height().ilog2() as usize
     }
 
-    pub fn new(num_rows: usize, num_col: usize, padding_strategy: InstancePaddingStrategy) -> Self {
-        let value = (0..num_rows * num_col)
+    pub fn new(
+        num_rows: usize,
+        num_cols: usize,
+        padding_strategy: InstancePaddingStrategy,
+    ) -> Self {
+        let num_row_padded = next_pow2_instance_padding(num_rows);
+        let value = (0..num_row_padded * num_cols)
             .into_par_iter()
             .map(|_| T::default())
             .collect();
         RowMajorMatrix {
-            inner: p3::matrix::dense::RowMajorMatrix::new(value, num_col),
+            inner: p3::matrix::dense::RowMajorMatrix::new(value, num_cols),
+            num_rows,
+            is_padded: matches!(padding_strategy, InstancePaddingStrategy::Default),
             padding_strategy,
         }
     }
 
     pub fn new_by_inner_matrix(
-        m: p3::matrix::dense::RowMajorMatrix<T>,
+        mut m: p3::matrix::dense::RowMajorMatrix<T>,
         padding_strategy: InstancePaddingStrategy,
     ) -> Self {
+        let num_rows = m.height();
+        let num_row_padded = next_pow2_instance_padding(num_rows);
+        if num_row_padded > m.height() {
+            m.pad_to_height(num_row_padded, T::default());
+        }
         RowMajorMatrix {
             inner: m,
+            num_rows,
+            is_padded: matches!(padding_strategy, InstancePaddingStrategy::Default),
             padding_strategy,
         }
     }
@@ -96,47 +119,50 @@ impl<T: Sized + Sync + Clone + Send + Copy + Default + PrimeCharacteristicRing> 
     }
 
     pub fn num_instances(&self) -> usize {
-        self.inner.height()
+        self.num_rows
     }
 
     pub fn iter_rows(&self) -> Chunks<T> {
-        self.inner.values.chunks(self.inner.width)
+        self.inner.values[..self.num_instances() * self.n_col()].chunks(self.inner.width)
     }
 
     pub fn iter_mut(&mut self) -> ChunksMut<T> {
-        self.inner.values.chunks_mut(self.inner.width)
+        let max_range = self.num_instances() * self.n_col();
+        self.inner.values[..max_range].chunks_mut(self.inner.width)
     }
 
     pub fn par_batch_iter_mut(&mut self, num_rows: usize) -> rayon::slice::ChunksMut<T> {
-        self.inner
-            .values
-            .par_chunks_mut(num_rows * self.inner.width)
+        let max_range = self.num_instances() * self.n_col();
+        self.inner.values[..max_range].par_chunks_mut(num_rows * self.inner.width)
     }
 
-    // Returns column number `column`, padded appropriately according to the stored strategy
-    pub fn column_padded(&self, column: usize) -> Vec<T> {
-        let n_column = self.n_col();
+    pub fn padding_by_strategy(&mut self) {
         let num_instances = self.num_instances();
-        let num_padding_instances = self.num_padding_instances();
+        let start_index = self.num_instances() * self.n_col();
 
-        let padding_iter = (num_instances..num_instances + num_padding_instances).map(|i| {
-            match &self.padding_strategy {
-                InstancePaddingStrategy::Custom(fun) => T::from_u64(fun(i as u64, column as u64)),
-                InstancePaddingStrategy::RepeatLast if num_instances > 0 => {
-                    self[num_instances - 1][column]
+        match &self.padding_strategy {
+            InstancePaddingStrategy::Default => (),
+            InstancePaddingStrategy::RepeatLast => {
+                if num_instances == 0 {
+                    return;
                 }
-                _ => T::default(),
+                let padding_vec = self[num_instances - 1].to_vec();
+                self.inner.values[start_index..]
+                    .par_chunks_mut(self.inner.width)
+                    .for_each(|instance| instance.copy_from_slice(&padding_vec));
             }
-        });
-
-        self.inner
-            .values
-            .iter()
-            .skip(column)
-            .step_by(n_column)
-            .copied()
-            .chain(padding_iter)
-            .collect::<Vec<_>>()
+            InstancePaddingStrategy::Custom(fun) => {
+                self.inner.values[start_index..]
+                    .par_chunks_mut(self.inner.width)
+                    .enumerate()
+                    .for_each(|(i, instance)| {
+                        instance.iter_mut().enumerate().for_each(|(j, v)| {
+                            *v = T::from_u64(fun((start_index + i) as u64, j as u64));
+                        })
+                    });
+            }
+        };
+        self.is_padded = true;
     }
 }
 
@@ -144,11 +170,21 @@ impl<F: Field + PrimeCharacteristicRing> RowMajorMatrix<F> {
     pub fn to_mles<E: ff_ext::ExtensionField<BaseField = F>>(
         &self,
     ) -> Vec<DenseMultilinearExtension<E>> {
+        debug_assert!(self.is_padded);
         let n_column = self.inner.width;
         (0..n_column)
             .into_par_iter()
-            .map(|i| self.column_padded(i).into_mle())
-            .collect()
+            .map(|i| {
+                self.inner
+                    .values
+                    .iter()
+                    .skip(i)
+                    .step_by(n_column)
+                    .copied()
+                    .collect::<Vec<_>>()
+                    .into_mle()
+            })
+            .collect::<Vec<_>>()
     }
 }
 
