fd571cf241286445b18d7ad33bd36e075923b835
Ming <hero78119@gmail.com>
2025-03-25 15:52:50 +0700

    field agnostic refactor (part 1) (#875)
    extract from #873, refactor ceno stark to field agnostic

--- ff_ext/src/poseidon.rs
+++ ff_ext/src/poseidon.rs
@@ -1,10 +1,22 @@
-use p3::{field::PrimeField, symmetric::CryptographicPermutation};
+use p3::{challenger::FieldChallenger, field::PrimeField};
 
-use crate::SmallField;
+use crate::{ExtensionField, SmallField};
+
+pub trait FieldChallengerExt<F: PoseidonField>: FieldChallenger<F> {
+    fn observe_ext_slice<E: ExtensionField<BaseField = F>>(&mut self, exts: &[E]) {
+        exts.iter()
+            .for_each(|ext| self.observe_slice(ext.as_basis_coefficients_slice()));
+    }
+
+    fn sample_ext_vec<E: ExtensionField<BaseField = F>>(&mut self, n: usize) -> Vec<E> {
+        (0..n).map(|_| self.sample_algebra_element()).collect()
+    }
+}
 
 pub trait PoseidonField: PrimeField + SmallField {
-    type T: CryptographicPermutation<[Self; 8]>;
-    fn get_perm() -> Self::T;
+    type P: Clone;
+    type T: FieldChallenger<Self> + Clone;
+    fn get_default_challenger() -> Self::T;
 }
 
 pub(crate) fn new_array<const N: usize, F: PrimeField>(input: [u64; N]) -> [F; N] {
