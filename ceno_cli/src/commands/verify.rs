ebb7fdefd651558a99dd55f35ee6582745c3dbf8
lightsing <light.tsing@gmail.com>
2025-04-23 16:18:49 +0800

    impl pcs and field selection

--- ceno_cli/src/commands/verify.rs
+++ ceno_cli/src/commands/verify.rs
@@ -1,16 +1,26 @@
 use crate::utils::print_cargo_message;
 use anyhow::{Context, bail};
 use ceno_zkvm::{
-    e2e::{E, Pcs, verify},
+    e2e::{FieldType, PcsKind, verify},
     scheme::{ZKVMProof, verifier::ZKVMVerifier},
     structs::ZKVMVerifyingKey,
 };
 use clap::Parser;
+use ff_ext::{BabyBearExt4, ExtensionField, GoldilocksExt2};
+use mpcs::{Basefold, BasefoldRSParams, PolynomialCommitmentScheme};
+use serde::Serialize;
 use std::{fs::File, path::PathBuf};
 
 #[derive(Parser)]
 #[command(name = "run", about = "Verify a Ceno proof")]
 pub struct VerifyCmd {
+    /// The polynomial commitment scheme to use.
+    #[arg(long, value_enum, default_value_t = PcsKind::default())]
+    pcs: PcsKind,
+    /// The field to use, eg. goldilocks
+    #[arg(long, value_enum, default_value_t = FieldType::default())]
+    field: FieldType,
+
     /// Path to the serialized proof file
     #[clap(long)]
     proof: PathBuf,
@@ -21,42 +31,58 @@ pub struct VerifyCmd {
 
 impl VerifyCmd {
     pub fn run(self) -> anyhow::Result<()> {
-        let start = std::time::Instant::now();
-        let zkvm_proof: ZKVMProof<E, Pcs> = bincode::deserialize_from(
-            File::open(&self.proof).context("Failed to open proof file")?,
-        )
-        .context("Failed to deserialize proof file")?;
-        print_cargo_message(
-            "Loaded",
-            format_args!(
-                "proof from {} in {:.2}s",
-                self.proof.canonicalize().unwrap().display(),
-                start.elapsed().as_secs_f32()
-            ),
-        );
-
-        let start = std::time::Instant::now();
-        let vk: ZKVMVerifyingKey<E, Pcs> =
-            bincode::deserialize_from(File::open(&self.vk).context("Failed to open vk file")?)
-                .context("Failed to deserialize vk file")?;
-        print_cargo_message(
-            "Loaded",
-            format_args!(
-                "verifying key from {} in {:.2}s",
-                self.vk.canonicalize().unwrap().display(),
-                start.elapsed().as_secs_f32()
-            ),
-        );
-
-        let start = std::time::Instant::now();
-        let verifier = ZKVMVerifier::new(vk);
-        if let Err(e) = verify(&zkvm_proof, &verifier) {
-            bail!("Verification failed: {e:?}");
+        match (self.pcs, self.field) {
+            (PcsKind::Basefold, FieldType::Goldilocks) => {
+                run_inner::<GoldilocksExt2, Basefold<GoldilocksExt2, BasefoldRSParams>>(self)
+            }
+            (PcsKind::Basefold, FieldType::BabyBear) => {
+                run_inner::<BabyBearExt4, Basefold<BabyBearExt4, BasefoldRSParams>>(self)
+            }
+            (PcsKind::Whir, FieldType::Goldilocks) => todo!(),
+            (PcsKind::Whir, FieldType::BabyBear) => todo!(),
         }
-        print_cargo_message(
-            "Verified",
-            format_args!("in {:.2}s", start.elapsed().as_secs_f32()),
-        );
-        Ok(())
     }
+}
+
+fn run_inner<E: ExtensionField, PCS: PolynomialCommitmentScheme<E> + Serialize>(
+    args: VerifyCmd,
+) -> anyhow::Result<()> {
+    let start = std::time::Instant::now();
+
+    let zkvm_proof: ZKVMProof<E, PCS> =
+        bincode::deserialize_from(File::open(&args.proof).context("Failed to open proof file")?)
+            .context("Failed to deserialize proof file")?;
+    print_cargo_message(
+        "Loaded",
+        format_args!(
+            "proof from {} in {:.2}s",
+            args.proof.canonicalize().unwrap().display(),
+            start.elapsed().as_secs_f32()
+        ),
+    );
+
+    let start = std::time::Instant::now();
+    let vk: ZKVMVerifyingKey<E, PCS> =
+        bincode::deserialize_from(File::open(&args.vk).context("Failed to open vk file")?)
+            .context("Failed to deserialize vk file")?;
+    print_cargo_message(
+        "Loaded",
+        format_args!(
+            "verifying key from {} in {:.2}s",
+            args.vk.canonicalize().unwrap().display(),
+            start.elapsed().as_secs_f32()
+        ),
+    );
+
+    let start = std::time::Instant::now();
+    let verifier = ZKVMVerifier::new(vk);
+    if let Err(e) = verify(&zkvm_proof, &verifier) {
+        bail!("Verification failed: {e:?}");
+    }
+
+    print_cargo_message(
+        "Verified",
+        format_args!("in {:.2}s", start.elapsed().as_secs_f32()),
+    );
+    Ok(())
 }
--- ceno_zkvm/src/bin/e2e.rs
+++ ceno_zkvm/src/bin/e2e.rs
@@ -1,12 +1,20 @@
-use ceno_emul::{IterAddresses, Program, WORD_SIZE, Word};
+use ceno_emul::{IterAddresses, Platform, Program, WORD_SIZE, Word};
 use ceno_host::{CenoStdin, memory_from_file};
 use ceno_zkvm::{
-    e2e::{B, Checkpoint, E, Pcs, Preset, run_e2e_with_checkpoint, setup_platform, verify},
-    scheme::{ZKVMProof, constants::MAX_NUM_VARIABLES, verifier::ZKVMVerifier},
+    e2e::{
+        Checkpoint, FieldType, PcsKind, Preset, run_e2e_with_checkpoint, setup_platform, verify,
+    },
+    scheme::{
+        ZKVMProof, constants::MAX_NUM_VARIABLES, mock_prover::LkMultiplicityKey,
+        verifier::ZKVMVerifier,
+    },
     with_panic_hook,
 };
 use clap::Parser;
+use ff_ext::{BabyBearExt4, ExtensionField, GoldilocksExt2};
+use mpcs::{Basefold, BasefoldRSParams, PolynomialCommitmentScheme};
 use p3::field::PrimeCharacteristicRing;
+use serde::{Serialize, de::DeserializeOwned};
 use std::{fs, panic, panic::AssertUnwindSafe, path::PathBuf};
 use tracing::{error, level_filters::LevelFilter};
 use tracing_forest::ForestLayer;
@@ -29,11 +37,11 @@ struct Args {
     elf: PathBuf,
     /// The path to the proof file to write.
     #[arg(default_value = "proof.bin")]
-    proof_file: String,
+    proof_file: PathBuf,
 
     /// The path to the verification key file to write.
     #[arg(default_value = "vk.bin")]
-    vk_file: String,
+    vk_file: PathBuf,
 
     /// The maximum number of steps to execute the program.
     #[arg(short, long)]
@@ -47,6 +55,13 @@ struct Args {
     /// The preset configuration to use.
     #[arg(short, long, value_enum, default_value_t = Preset::Ceno)]
     platform: Preset,
+
+    /// The polynomial commitment scheme to use.
+    #[arg(long, value_enum, default_value_t = PcsKind::default())]
+    pcs: PcsKind,
+    /// The field to use, eg. goldilocks
+    #[arg(long, value_enum, default_value_t = FieldType::default())]
+    field: FieldType,
 
     /// Hints: prover-private unconstrained input.
     /// This is a raw file mapped as a memory segment.
@@ -183,13 +198,57 @@ fn main() {
 
     let max_steps = args.max_steps.unwrap_or(usize::MAX);
 
-    let ((zkvm_proof, vk), _) = run_e2e_with_checkpoint::<E, Pcs>(
+    match (args.pcs, args.field) {
+        (PcsKind::Basefold, FieldType::Goldilocks) => {
+            run_inner::<GoldilocksExt2, Basefold<GoldilocksExt2, BasefoldRSParams>>(
+                program,
+                platform,
+                hints,
+                public_io,
+                max_steps,
+                args.max_num_variables,
+                args.proof_file,
+                args.vk_file,
+            )
+        }
+        (PcsKind::Basefold, FieldType::BabyBear) => {
+            run_inner::<BabyBearExt4, Basefold<BabyBearExt4, BasefoldRSParams>>(
+                program,
+                platform,
+                hints,
+                public_io,
+                max_steps,
+                args.max_num_variables,
+                args.proof_file,
+                args.vk_file,
+            )
+        }
+        (PcsKind::Whir, FieldType::Goldilocks) => todo!(),
+        (PcsKind::Whir, FieldType::BabyBear) => todo!(),
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
+fn run_inner<
+    E: ExtensionField + LkMultiplicityKey + DeserializeOwned,
+    PCS: PolynomialCommitmentScheme<E> + Serialize + 'static,
+>(
+    program: Program,
+    platform: Platform,
+    hints: Vec<u32>,
+    public_io: Vec<u32>,
+    max_steps: usize,
+    max_num_variables: usize,
+    proof_file: PathBuf,
+    vk_file: PathBuf,
+) {
+    let ((zkvm_proof, vk), _) = run_e2e_with_checkpoint::<E, PCS>(
         program,
         platform,
         hints,
         public_io,
         max_steps,
-        args.max_num_variables,
+        max_num_variables,
         Checkpoint::PrepSanityCheck,
     );
 
@@ -197,22 +256,24 @@ fn main() {
     let vk = vk.expect("PrepSanityCheck should yield vk.");
 
     let proof_bytes = bincode::serialize(&zkvm_proof).unwrap();
-    std::fs::write(&args.proof_file, proof_bytes).unwrap();
+    fs::write(&proof_file, proof_bytes).unwrap();
     let vk_bytes = bincode::serialize(&vk).unwrap();
-    std::fs::write(&args.vk_file, vk_bytes).unwrap();
+    fs::write(&vk_file, vk_bytes).unwrap();
 
     let verifier = ZKVMVerifier::new(vk);
     verify(&zkvm_proof, &verifier).expect("Verification failed");
-    // FIXME: it is a bit wired, let us move it else where later.
     soundness_test(zkvm_proof, &verifier);
 }
 
-fn soundness_test(mut zkvm_proof: ZKVMProof<E, Pcs>, verifier: &ZKVMVerifier<E, Pcs>) {
+fn soundness_test<E: ExtensionField, Pcs: PolynomialCommitmentScheme<E>>(
+    mut zkvm_proof: ZKVMProof<E, Pcs>,
+    verifier: &ZKVMVerifier<E, Pcs>,
+) {
     // do sanity check
     let transcript = Transcript::new(b"riscv");
     // change public input maliciously should cause verifier to reject proof
-    zkvm_proof.raw_pi[0] = vec![B::ONE];
-    zkvm_proof.raw_pi[1] = vec![B::ONE];
+    zkvm_proof.raw_pi[0] = vec![E::BaseField::ONE];
+    zkvm_proof.raw_pi[1] = vec![E::BaseField::ONE];
 
     // capture panic message, if have
     let result = with_panic_hook(Box::new(|_info| ()), || {
