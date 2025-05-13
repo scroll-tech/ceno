34644f9b7aef041c29f355c09f4e8c49fd074eb6
sm.wu <hero78119@gmail.com>
2025-05-13 20:44:27 +0800

    fix benchmark usage

--- ceno_zkvm/benches/alloc.rs
+++ ceno_zkvm/benches/alloc.rs
@@ -1,4 +1,4 @@
 // Use jemalloc as global allocator for performance
-#[cfg(all(feature = "jemalloc", unix, not(test)))]
+#[cfg(all(feature = "jemalloc", unix))]
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
