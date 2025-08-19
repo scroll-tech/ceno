d4178eb3362f2d3947ee293bf7d9daa881940ff8
sm.wu <hero78119@gmail.com>
2025-08-19 15:59:53 +0800

    migrate jal/jalr

--- ceno_zkvm/src/instructions/riscv/jump.rs
+++ ceno_zkvm/src/instructions/riscv/jump.rs
@@ -2,14 +2,21 @@
 mod jal;
 #[cfg(feature = "u16limb_circuit")]
 mod jal_v2;
+
+#[cfg(not(feature = "u16limb_circuit"))]
 mod jalr;
+#[cfg(feature = "u16limb_circuit")]
+mod jalr_v2;
 
 #[cfg(not(feature = "u16limb_circuit"))]
 pub use jal::JalInstruction;
 #[cfg(feature = "u16limb_circuit")]
 pub use jal_v2::JalInstruction;
 
+#[cfg(not(feature = "u16limb_circuit"))]
 pub use jalr::JalrInstruction;
+#[cfg(feature = "u16limb_circuit")]
+pub use jalr_v2::JalrInstruction;
 
 #[cfg(test)]
 mod test;
--- ceno_zkvm/src/instructions/riscv/jump/test.rs
+++ ceno_zkvm/src/instructions/riscv/jump/test.rs
@@ -1,37 +1,46 @@
-use ceno_emul::{ByteAddr, Change, InsnKind, PC_STEP_SIZE, StepRecord, Word, encode_rv32};
-use ff_ext::GoldilocksExt2;
-
+use super::{JalInstruction, JalrInstruction};
 use crate::{
+    Value,
     circuit_builder::{CircuitBuilder, ConstraintSystem},
-    instructions::Instruction,
+    instructions::{Instruction, riscv::constants::UInt},
     scheme::mock_prover::{MOCK_PC_START, MockProver},
     structs::ProgramParams,
 };
-
-use super::{JalInstruction, JalrInstruction};
+use ceno_emul::{ByteAddr, Change, InsnKind, PC_STEP_SIZE, StepRecord, Word, encode_rv32};
+#[cfg(feature = "u16limb_circuit")]
+use ff_ext::BabyBearExt4;
+use ff_ext::{ExtensionField, GoldilocksExt2};
+use gkr_iop::circuit_builder::DebugIndex;
 
 #[test]
 fn test_opcode_jal() {
-    let mut cs = ConstraintSystem::<GoldilocksExt2>::new(|| "riscv");
+    verify_test_opcode_jal::<GoldilocksExt2>(-8);
+    verify_test_opcode_jal::<GoldilocksExt2>(8);
+
+    #[cfg(feature = "u16limb_circuit")]
+    {
+        verify_test_opcode_jal::<BabyBearExt4>(-8);
+        verify_test_opcode_jal::<BabyBearExt4>(8);
+    }
+}
+
+fn verify_test_opcode_jal<E: ExtensionField>(pc_offset: i32) {
+    let mut cs = ConstraintSystem::<E>::new(|| "riscv");
     let mut cb = CircuitBuilder::new(&mut cs);
     let config = cb
         .namespace(
             || "jal",
             |cb| {
-                let config = JalInstruction::<GoldilocksExt2>::construct_circuit(
-                    cb,
-                    &ProgramParams::default(),
-                );
+                let config = JalInstruction::<E>::construct_circuit(cb, &ProgramParams::default());
                 Ok(config)
             },
         )
         .unwrap()
         .unwrap();
 
-    let pc_offset: i32 = -8i32;
     let new_pc: ByteAddr = ByteAddr(MOCK_PC_START.0.wrapping_add_signed(pc_offset));
     let insn_code = encode_rv32(InsnKind::JAL, 0, 0, 4, pc_offset);
-    let (raw_witin, lkm) = JalInstruction::<GoldilocksExt2>::assign_instances(
+    let (raw_witin, lkm) = JalInstruction::<E>::assign_instances(
         &config,
         cb.cs.num_witin as usize,
         cb.cs.num_structural_witin as usize,
@@ -45,33 +54,68 @@ fn test_opcode_jal() {
     )
     .unwrap();
 
+    // verify rd_written
+    let expected_rd_written = UInt::from_const_unchecked(
+        Value::new_unchecked(MOCK_PC_START.0 + PC_STEP_SIZE as u32)
+            .as_u16_limbs()
+            .to_vec(),
+    );
+    let rd_written_expr = cb.get_debug_expr(DebugIndex::RdWrite as usize)[0].clone();
+    cb.require_equal(
+        || "assert_rd_written",
+        rd_written_expr,
+        expected_rd_written.value(),
+    )
+    .unwrap();
+
     MockProver::assert_satisfied_raw(&cb, raw_witin, &[insn_code], None, Some(lkm));
 }
 
 #[test]
 fn test_opcode_jalr() {
-    let mut cs = ConstraintSystem::<GoldilocksExt2>::new(|| "riscv");
+    verify_test_opcode_jalr::<GoldilocksExt2>(100, 3);
+    verify_test_opcode_jalr::<GoldilocksExt2>(100, -3);
+
+    #[cfg(feature = "u16limb_circuit")]
+    {
+        verify_test_opcode_jalr::<BabyBearExt4>(100, 3);
+        verify_test_opcode_jalr::<BabyBearExt4>(100, -3);
+    }
+}
+
+fn verify_test_opcode_jalr<E: ExtensionField>(rs1_read: Word, imm: i32) {
+    let mut cs = ConstraintSystem::<E>::new(|| "riscv");
     let mut cb = CircuitBuilder::new(&mut cs);
     let config = cb
         .namespace(
             || "jalr",
             |cb| {
-                let config = JalrInstruction::<GoldilocksExt2>::construct_circuit(
-                    cb,
-                    &ProgramParams::default(),
-                );
+                let config = JalrInstruction::<E>::construct_circuit(cb, &ProgramParams::default());
                 Ok(config)
             },
         )
         .unwrap()
         .unwrap();
 
-    let imm = -15i32;
-    let rs1_read: Word = 100u32;
+    // trim lower bit to 0
     let new_pc: ByteAddr = ByteAddr(rs1_read.wrapping_add_signed(imm) & (!1));
     let insn_code = encode_rv32(InsnKind::JALR, 2, 0, 4, imm);
 
-    let (raw_witin, lkm) = JalrInstruction::<GoldilocksExt2>::assign_instances(
+    // verify rd_written
+    let expected_rd_written = UInt::from_const_unchecked(
+        Value::new_unchecked(MOCK_PC_START.0 + PC_STEP_SIZE as u32)
+            .as_u16_limbs()
+            .to_vec(),
+    );
+    let rd_written_expr = cb.get_debug_expr(DebugIndex::RdWrite as usize)[0].clone();
+    cb.require_equal(
+        || "assert_rd_written",
+        rd_written_expr,
+        expected_rd_written.value(),
+    )
+    .unwrap();
+
+    let (raw_witin, lkm) = JalrInstruction::<E>::assign_instances(
         &config,
         cb.cs.num_witin as usize,
         cb.cs.num_structural_witin as usize,
