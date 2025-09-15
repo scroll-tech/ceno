b5d69ff6dfda2404ad834f6b9556b9c7e64d13be
dreamATD <tianyi.liu.08@gmail.com>
2025-09-15 03:20:28 -0500

    Add weierstrass double circuit

--- ceno_zkvm/src/precompiles/weierstrass.rs
+++ ceno_zkvm/src/precompiles/weierstrass.rs
@@ -2,6 +2,7 @@ use generic_array::GenericArray;
 use sp1_curves::params::NumWords;
 
 pub mod weierstrass_add;
+pub mod weierstrass_double;
 
 #[derive(Clone, Default, Debug)]
 pub struct EllipticCurveAddInstance<P: NumWords> {
@@ -9,4 +10,10 @@ pub struct EllipticCurveAddInstance<P: NumWords> {
     pub p: GenericArray<u32, P::WordsCurvePoint>,
     /// The second point as a list of words.
     pub q: GenericArray<u32, P::WordsCurvePoint>,
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct EllipticCurveDoubleInstance<P: NumWords> {
+    /// The point as a list of words.
+    pub p: GenericArray<u32, P::WordsCurvePoint>,
 }
