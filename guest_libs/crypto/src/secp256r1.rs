2d7d4f79f99c86152307e5f59e103f800974458d
lightsing <light.tsing@gmail.com>
2025-09-26 13:20:38 +0800

    add skeleton

--- guest_libs/crypto/src/secp256r1.rs
+++ guest_libs/crypto/src/secp256r1.rs
@@ -1,5 +1,5 @@
 /// secp256r1 (P-256) signature verification.
 #[inline]
-fn secp256r1_verify_signature(msg: &[u8; 32], sig: &[u8; 64], pk: &[u8; 64]) -> bool {
+pub fn secp256r1_verify_signature(_msg: &[u8; 32], _sig: &[u8; 64], _pk: &[u8; 64]) -> bool {
     unimplemented!()
 }
