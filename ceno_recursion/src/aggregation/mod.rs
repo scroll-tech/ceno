03ad436a85d3e7a232abef332b88e75b645e2851
Ray Gao <qg2153@columbia.edu>
2025-11-23 19:40:49 -0500

    ignore test

--- ceno_recursion/src/aggregation/mod.rs
+++ ceno_recursion/src/aggregation/mod.rs
@@ -540,7 +540,7 @@ mod tests {
     }
 
     #[test]
-    // #[ignore = "need to generate proof first"]
+    #[ignore = "need to generate proof first"]
     pub fn test_single() {
         let stack_size = 256 * 1024 * 1024; // 64 MB
 
