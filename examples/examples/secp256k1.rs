0a1608b5cdd6bc873b5d0fc4cdf2989909f163e1
sm.wu <hero78119@gmail.com>
2025-11-24 14:59:58 +0800

    clippy fix

--- examples/examples/secp256k1.rs
+++ examples/examples/secp256k1.rs
@@ -1,15 +1,15 @@
-
 extern crate ceno_rt;
-
-use k256::{ProjectivePoint, Scalar};
-use k256::elliptic_curve::Group;
-use k256::elliptic_curve::ops::MulByGenerator;
+#[allow(unused_imports)]
+use k256::{
+    ProjectivePoint, Scalar,
+    elliptic_curve::{Group, ops::MulByGenerator},
+};
 
 fn main() {
     let scalar = Scalar::from(5u64);
     let a = ProjectivePoint::mul_by_generator(&scalar);
     let _ = a.double(); // -> syscall_secp256k1_double
-    
+
     let scalar = Scalar::from(6u64);
     let b = ProjectivePoint::mul_by_generator(&scalar);
     let _ = a + b; // -> syscall_secp256k1_add
