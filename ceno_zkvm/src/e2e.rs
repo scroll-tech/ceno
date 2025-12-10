ccf788323a51617fcee4c308157fe78c3114e264
sm.wu <hero78119@gmail.com>
2025-12-10 22:03:29 +0800

    log num_shards

--- ceno_zkvm/src/e2e.rs
+++ ceno_zkvm/src/e2e.rs
@@ -1543,7 +1543,7 @@ fn create_proofs_helper<
     target_shard_id: Option<usize>,
 ) -> Vec<ZKVMProof<E, PCS>> {
     let ctx = prover.pk.program_ctx.as_ref().unwrap();
-    info_span!("app_prove.inner").in_scope(|| {
+    let proofs = info_span!("app_prove.inner").in_scope(|| {
         #[cfg(feature = "gpu")]
         {
             use crossbeam::channel;
@@ -1649,7 +1649,9 @@ fn create_proofs_helper<
                 })
                 .collect_vec()
         }
-    })
+    });
+    metrics::gauge!("num_shards").set(proofs.len() as f64);
+    proofs
 }
 
 pub fn run_e2e_verify<E: ExtensionField, PCS: PolynomialCommitmentScheme<E>>(
