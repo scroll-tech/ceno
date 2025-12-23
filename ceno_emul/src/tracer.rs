a81f58ebeb686bc2abc6fc58d2fbe001a31c4f4f
sm.wu <hero78119@gmail.com>
2025-12-23 16:53:14 +0800

    fix bug

--- ceno_emul/src/tracer.rs
+++ ceno_emul/src/tracer.rs
@@ -672,18 +672,18 @@ impl FullTracer {
                 if addr < *min_addr {
                     *min_addr = addr; // start is inclusive
                 }
-            }
-            if start_addr.baddr().0 == self.platform.heap.start {
-                let access_end = addr + WordAddr::from(WORD_SIZE as u32);
-                let access_end_byte = access_end.baddr();
-                if access_end_byte > self.max_heap_addr_access {
-                    self.max_heap_addr_access = access_end_byte;
-                }
-            } else if start_addr.baddr().0 == self.platform.hints.start {
-                let access_end = addr + WordAddr::from(WORD_SIZE as u32);
-                let access_end_byte = access_end.baddr();
-                if access_end_byte > self.max_hint_addr_access {
-                    self.max_hint_addr_access = access_end_byte;
+                if start_addr.baddr().0 == self.platform.heap.start {
+                    let access_end = addr + WordAddr::from(WORD_SIZE as u32);
+                    let access_end_byte = access_end.baddr();
+                    if access_end_byte > self.max_heap_addr_access {
+                        self.max_heap_addr_access = access_end_byte;
+                    }
+                } else if start_addr.baddr().0 == self.platform.hints.start {
+                    let access_end = addr + WordAddr::from(WORD_SIZE as u32);
+                    let access_end_byte = access_end.baddr();
+                    if access_end_byte > self.max_hint_addr_access {
+                        self.max_hint_addr_access = access_end_byte;
+                    }
                 }
             }
         }
