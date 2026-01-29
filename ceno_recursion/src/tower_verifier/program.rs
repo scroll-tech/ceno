67092deca647f5220d4482383e9e7635bdac0997
xkx <xiakunxian130@gmail.com>
2026-01-29 17:21:21 +0800

    feat(recursion): reduce cycles in ceno proof loading (#1248)
    ## Summary
    
    - [x] rely on https://github.com/scroll-tech/openvm/pull/30.
    - [x] we read `prod_evals` from hint space inside `NativeSumcheck` chip
    instead of moving them from hint stream to memory word by word.
    
    ### Performance
    
    verifying the 6th shard of block `23817600`, the total cells and
    instructions_executed before and after this PR.
    
    |  | total cells | instructions_executed |
    | - | - | - |
    | before | 2B | 9.5M |
    | after | 1.3B | 6.7M |

--- ceno_recursion/src/tower_verifier/program.rs
+++ ceno_recursion/src/tower_verifier/program.rs
@@ -383,8 +383,8 @@ pub fn verify_tower_proof<C: Config>(
         builder.sumcheck_layer_eval(
             &input_ctx,
             &challenges,
-            &proof.prod_specs_eval.data,
-            &proof.logup_specs_eval.data,
+            proof.prod_specs_eval.data.id.get_var(),
+            proof.logup_specs_eval.data.id.get_var(),
             &next_layer_evals,
         );
         let expected_evaluation = builder.get(&next_layer_evals, 0);
@@ -422,8 +422,8 @@ pub fn verify_tower_proof<C: Config>(
         builder.sumcheck_layer_eval(
             &input_ctx,
             &challenges,
-            &proof.prod_specs_eval.data,
-            &proof.logup_specs_eval.data,
+            proof.prod_specs_eval.data.id.get_var(),
+            proof.logup_specs_eval.data.id.get_var(),
             &next_layer_evals,
         );
 
