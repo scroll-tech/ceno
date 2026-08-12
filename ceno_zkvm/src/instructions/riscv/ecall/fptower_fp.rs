5913ad669f80da86b8d0ca22bd3915cfc2152f55
sm.wu <hero78119@gmail.com>
2026-08-12 15:03:46 +0800

    Lower production zerochecks to degree 3

--- ceno_zkvm/src/instructions/riscv/ecall/fptower_fp.rs
+++ ceno_zkvm/src/instructions/riscv/ecall/fptower_fp.rs
@@ -97,7 +97,7 @@ impl<E: ExtensionField, P: FpOpField + FpAddSpec + NumWords> Instruction<E>
         cb: &mut CircuitBuilder<E>,
         _param: &ProgramParams,
     ) -> Result<(Self::InstructionConfig, GKRCircuit<E>), ZKVMError> {
-        build_fp_op_circuit::<E, P>(cb, P::SYSCALL_CODE, "fp_add")
+        build_fp_op_circuit::<E, P>(cb, P::SYSCALL_CODE, "fp_add", FieldOperation::Add)
     }
 
     fn generate_fixed_traces(
@@ -167,7 +167,7 @@ impl<E: ExtensionField, P: FpOpField + FpMulSpec + NumWords> Instruction<E>
         cb: &mut CircuitBuilder<E>,
         _param: &ProgramParams,
     ) -> Result<(Self::InstructionConfig, GKRCircuit<E>), ZKVMError> {
-        build_fp_op_circuit::<E, P>(cb, P::SYSCALL_CODE, "fp_mul")
+        build_fp_op_circuit::<E, P>(cb, P::SYSCALL_CODE, "fp_mul", FieldOperation::Mul)
     }
 
     fn generate_fixed_traces(
@@ -214,6 +214,7 @@ fn build_fp_op_circuit<E: ExtensionField, P: FpOpField + NumWords>(
     cb: &mut CircuitBuilder<E>,
     syscall_code: u32,
     layer_name: &str,
+    op: FieldOperation,
 ) -> Result<(EcallFpOpConfig<E, P>, GKRCircuit<E>), ZKVMError> {
     let vm_state = StateInOut::construct_circuit(cb, false)?;
 
@@ -253,7 +254,7 @@ fn build_fp_op_circuit<E: ExtensionField, P: FpOpField + NumWords>(
         0.into(),
     ))?;
 
-    let mut layout = <FpOpLayout<E, P> as ProtocolBuilder<E>>::build_layer_logic(cb, ())?;
+    let mut layout = FpOpLayout::<E, P>::build_fixed_op(cb, op)?;
 
     let mut mem_rw = izip!(&layout.input32_exprs[0], &layout.output32_exprs)
         .enumerate()
